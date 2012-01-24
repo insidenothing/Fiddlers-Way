848c670a2fd93931627c568f1d513f3ffdb29375
Patrick McGuire <insidenothing@gmail.com>
2012-01-24 11:43:29 -0500

    revert

--- index.php
+++ index.php
@@ -14,23 +14,39 @@
 
 get_header(); ?>
 
+		<div id="primary">
+			<div id="content" role="main">
 
-<table border="5" width="100%">
-	<tr>
-		<td colspan="3"><div><?php twentyeleven_content_nav( 'nav-above' ); ?></div></td>
-	</tr>
-	<tr>
-		<td><div><?php get_sidebar(); ?></div></td>
-		<td>
-			<div>
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', get_post_format() ); ?>
-			<?php endwhile; ?>
-			<?php twentyeleven_content_nav( 'nav-below' ); ?>
-			</div>
-		</td>
-		<td><div><?php get_sidebar(); ?></div></td>
-	</tr>
-</table>
+			<?php if ( have_posts() ) : ?>
 
+				<?php twentyeleven_content_nav( 'nav-above' ); ?>
+
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', get_post_format() ); ?>
+
+				<?php endwhile; ?>
+
+				<?php twentyeleven_content_nav( 'nav-below' ); ?>
+
+			<?php else : ?>
+
+				<article id="post-0" class="post no-results not-found">
+					<header class="entry-header">
+						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+					</header><!-- .entry-header -->
+
+					<div class="entry-content">
+						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
+						<?php get_search_form(); ?>
+					</div><!-- .entry-content -->
+				</article><!-- #post-0 -->
+
+			<?php endif; ?>
+
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
