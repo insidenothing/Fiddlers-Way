bc7ca54a34f0f6f03cd8536a254ad89121dacedb
Patrick McGuire <insidenothing@gmail.com>
2012-01-27 11:30:58 -0500

    style

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -46,16 +46,24 @@
 					src="http://www.youtube.com/embed/J1A4PUZfVg8" frameborder="0"
 					allowfullscreen></iframe>
 				<h1>IPO Preview</h1>
-				
-				<div>Is the Renewable Industry Renewable?</div>
-				<div>Based in Ames, Iowa, Renewable Energy Group, (REGI), scheduled, as the first initial public offering of 2012, a $100 million IPO with a market capitalization of $401 million at a price range mid-point of $14 for today, Thursday, January 19, 2011.  REGI priced last night (January 18) at $10, 28% below mid-range & now trades flat.  http://finance.yahoo.com/q?s=regi&ql=1  The stock opened on the Nasdaq at $10.10, 1% above its target offering of $10.00. A total of 7.2 million shares were sold, at prices well below the expected mid-point range of $13-$15 per share.
 
-REGI's outlook depends on politics, because – in our opinion – biodiesel without mandates and subsidies is not a viable market.  So we are neutral on REGI at $10 today.</div>
-				
+				<div class="title">Is the Renewable Industry Renewable?</div>
+				<div class="post">Based in Ames, Iowa, Renewable Energy Group, (REGI), scheduled,
+					as the first initial public offering of 2012, a $100 million IPO
+					with a market capitalization of $401 million at a price range
+					mid-point of $14 for today, Thursday, January 19, 2011. REGI priced
+					last night (January 18) at $10, 28% below mid-range & now trades
+					flat. http://finance.yahoo.com/q?s=regi&ql=1 The stock opened on
+					the Nasdaq at $10.10, 1% above its target offering of $10.00. A
+					total of 7.2 million shares were sold, at prices well below the
+					expected mid-point range of $13-$15 per share. REGI's outlook
+					depends on politics, because – in our opinion – biodiesel without
+					mandates and subsidies is not a viable market. So we are neutral on
+					REGI at $10 today.</div>
 			</td>
 			<td width='200px' valign="top" bgcolor="#e8e9ea">
 				<h1>Track Record</h1>
-				<div>TECHNOLOGY PLAY (1) One of our highest scored stocks of 2011
+				<div class="post">TECHNOLOGY PLAY (1) One of our highest scored stocks of 2011
 					was InvenSense (INVN) INVN priced at $7.50 November 15 and is now
 					up 93% to $14.51 in about two months.
 					http://finance.yahoo.com/q?s=INVN up93% in about two months It
@@ -73,7 +81,7 @@ REGI's outlook depends on politics, because – in our opinion – biodiesel without
 					http://gaskinsco.com/linkto-uan.shtml At the pre-IPO price range
 					mid-point of $13, UAN was expected to yield 14.8%.</div>
 				<h1>In The News</h1>
-				<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+				<div class="post">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
 					lobortis aliquet tortor, eu posuere tellus eleifend at. Quisque sed
 					justo velit, a congue justo. Sed commodo turpis id metus ultricies
 					ut egestas nisl ultrices. Ut urna turpis, pretium tincidunt
