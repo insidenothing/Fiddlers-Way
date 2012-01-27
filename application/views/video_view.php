ce035c63d3ab800aad64512852d1a552a3dc48a6
Patrick McGuire <insidenothing@gmail.com>
2012-01-27 15:50:18 -0500

    videos page

--- application/views/video_view.php
+++ application/views/video_view.php
@@ -1,1 +1,24 @@
-video view
+<h1>Most Recent Video Posts</h1>
+
+<center>
+	<iframe width="560" height="315"
+		src="http://www.youtube.com/embed/KsgGB9v5CTs" frameborder="0"
+		allowfullscreen></iframe>
+</center>
+
+<div class="vid-desc">"Definitely watch it... but in the near term, I
+	don't know how the stock is going to act... it may not hold up that
+	well in this market.."</div>
+
+<hr>
+<center>
+	<iframe width="560" height="315"
+		src="http://www.youtube.com/embed/B3an6Cnxs4A" frameborder="0"
+		allowfullscreen></iframe>
+</center>
+<hr>
+<center>
+	<iframe width="560" height="315"
+		src="http://www.youtube.com/embed/J1A4PUZfVg8" frameborder="0"
+		allowfullscreen></iframe>
+</center>
