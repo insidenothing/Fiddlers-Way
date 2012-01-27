63ec51a1898e23a483ee081869b418be0eb1b5c9
Patrick McGuire <insidenothing@gmail.com>
2012-01-27 11:27:01 -0500

    ipo preview

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,125 +1,105 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Welcome to Fiddlers Way</title>
-
+<meta charset="utf-8">
+<title>Welcome to Fiddlers Way</title>
+<link rel="stylesheet" type="text/css" href="/assets/css/common.css" />
+<script SRC="/assets/js/common.js" TYPE="text/javascript"></script>
 </head>
 <body>
+	<table width="100%" cellpadding="0" cellspacing="0" border="1"
+		align='center'>
+		<tr>
+			<td colspan="4" align="center"><a href="/"><img
+					src="/assets/images/fw2.jpg"> </a></td>
+		</tr>
+		<tr>
+			<td width='200px' valign="top" bgcolor="#e8e9ea">
+				<li>Newsletter</li>
+				<li>Sign In</li>
+				<div>
+					<img src="/assets/images/facebook_logo.gif">
+				</div>
+				<div>
+					<img src="/assets/images/twitter_logo.gif">
+				</div>
+				<div>
+					<img src="/assets/images/linkedin_logo.gif">
+				</div>
+				<div>
+					<img src="/assets/images/youtube_logo.gif">
+				</div>
+				<li>Twitter Feed</li>
+				<li>Tag Cloud</li>
+			</td>
+			<td width='400px' valign="top" bgcolor="#e8e9ea"><iframe width="560"
+					height="315" src="http://www.youtube.com/embed/KsgGB9v5CTs"
+					frameborder="0" allowfullscreen></iframe>
 
-<style>
-table{
-border-collapse:collapse;
-}
-body{
-background-image:url('/assets/images/wood7.bmp');
-background-attachment:fixed;
-}
-.violin{
-background-image:url('/assets/images/wood8.bmp');
-background-attachment:fixed;
-background-repeat:repeat-y;
-background-position: top right;
-}
-.vid-desc{
-padding:5px;
-border:solid 1px #999;
-}
-h1, a {
-    color: #002B54;
-    font-family: Georgia;
-    font-style: italic;
-    text-shadow: -1px -1px 1px #FFFFFF;
-}
-h3 {
-    color: #000000;
-    font-family: Georgia;
-    font-style: italic;
-    text-shadow: -1px -1px 1px #FFFFFF;
-}
+				<div class="vid-desc">"Definitely watch it... but in the near term,
+					I don't know how the stock is going to act... it may not hold up
+					that well in this market.."</div>
+				<h1>Most Recent Blog Posts</h1>
+				<h1>Most Recent Video Posts</h1> <iframe width="560" height="315"
+					src="http://www.youtube.com/embed/B3an6Cnxs4A" frameborder="0"
+					allowfullscreen></iframe> <iframe width="560" height="315"
+					src="http://www.youtube.com/embed/J1A4PUZfVg8" frameborder="0"
+					allowfullscreen></iframe>
+				<h1>IPO Preview</h1>
+				
+				<div>Is the Renewable Industry Renewable?</div>
+				<div>Based in Ames, Iowa, Renewable Energy Group, (REGI), scheduled, as the first initial public offering of 2012, a $100 million IPO with a market capitalization of $401 million at a price range mid-point of $14 for today, Thursday, January 19, 2011.  REGI priced last night (January 18) at $10, 28% below mid-range & now trades flat.  http://finance.yahoo.com/q?s=regi&ql=1  The stock opened on the Nasdaq at $10.10, 1% above its target offering of $10.00. A total of 7.2 million shares were sold, at prices well below the expected mid-point range of $13-$15 per share.
 
-p{
-    color: #000000;
-    font-family: Times New Roman;
-    text-shadow: -1px -1px 1px #FFFFFF;
-}
-</style>
+REGI's outlook depends on politics, because – in our opinion – biodiesel without mandates and subsidies is not a viable market.  So we are neutral on REGI at $10 today.</div>
+				
+			</td>
+			<td width='200px' valign="top" bgcolor="#e8e9ea">
+				<h1>Track Record</h1>
+				<div>TECHNOLOGY PLAY (1) One of our highest scored stocks of 2011
+					was InvenSense (INVN) INVN priced at $7.50 November 15 and is now
+					up 93% to $14.51 in about two months.
+					http://finance.yahoo.com/q?s=INVN up93% in about two months It
+					traded in the immediate aftermarket at$8.25, and is up 76% in the
+					aftermarket. INVN pre-IPOreport
+					http://gaskinsco.com/linkto-invn.shtml TWO HIGH YIELD PLAYS (2)
+					Another favorite IPO from 2011 is Chesapeake Granite Wash Trust
+					(CHKR) which IPO.d November 10, 2011 at $19 traded down a little
+					and today is $23.22, up 22%. And at $19 CHKR was projected pay a
+					yield in 2012 of 13% CHKR pre-IPOreport
+					http://gaskinsco.com/linkto-chkr.shtml (3) We always liked CVR
+					Partners (UAN), because of it yield and its business. UAN IPO.d
+					April 7, 2011 at $16, and now trades up 75% at $27.98.
+					http://finance.yahoo.com/q?s=uan&ql=1 UAN pre-IPOreport
+					http://gaskinsco.com/linkto-uan.shtml At the pre-IPO price range
+					mid-point of $13, UAN was expected to yield 14.8%.</div>
+				<h1>In The News</h1>
+				<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+					lobortis aliquet tortor, eu posuere tellus eleifend at. Quisque sed
+					justo velit, a congue justo. Sed commodo turpis id metus ultricies
+					ut egestas nisl ultrices. Ut urna turpis, pretium tincidunt
+					condimentum ac, facilisis vel enim. In placerat dolor ac felis
+					molestie hendrerit. Curabitur non urna eros. Cras tortor velit,
+					feugiat hendrerit euismod ornare, iaculis id mauris. Vivamus
+					placerat, lacus vel condimentum auctor, arcu nulla porttitor
+					turpis, sed suscipit nisi est quis arcu. Donec commodo sollicitudin
+					massa, ac mattis sapien dictum quis. Fusce felis metus, pretium eu
+					luctus sit amet, pellentesque eu justo. Cum sociis natoque
+					penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+					Morbi in mi et enim pharetra auctor eget vel libero. Pellentesque
+					blandit, turpis a facilisis dignissim, justo urna porttitor metus,
+					sit amet mollis libero ipsum faucibus odio. Sed felis turpis,
+					gravida ac venenatis vel, vestibulum vitae neque. Duis id augue
+					quis mi egestas suscipit sed eget ipsum. Phasellus vehicula
+					tincidunt facilisis.</div>
+			</td>
+			<td width='200px' class="violin">&nbsp</td>
+		</tr>
+	</table>
 
-<table width="100%" cellpadding="0" cellspacing="0" border="1" align='center'>
-	<tr>
-		<td colspan="4" align="center"><a href="/"><img src="/assets/images/fw2.jpg"></a></td>
-	</tr>
-	<tr>
-		<td width='200px' valign="top" bgcolor="#e8e9ea">
-		<li>Newsletter</li>
-		<li>Sign In</li>
-<div><img src="/assets/images/facebook_logo.gif"></div>
-<div><img src="/assets/images/twitter_logo.gif"></div>
-<div><img src="/assets/images/linkedin_logo.gif"></div>
-<div><img src="/assets/images/youtube_logo.gif"></div>
-		<li>Twitter Feed</li>
-		<li>Tag Cloud</li>
-		</td>
-		<td width='400px' valign="top" bgcolor="#e8e9ea">
-<iframe width="560" height="315" src="http://www.youtube.com/embed/KsgGB9v5CTs" frameborder="0" allowfullscreen></iframe>
-
-<div class="vid-desc">"Definitely watch it... but in the near term, I don't know how the stock is going to act... it may not hold up that well in this market.."</div>
-
-		
-<h1>Most Recent Blog Posts</h1>
-		<h1>Most Recent Video Posts</h1>
-	
-
-<iframe width="560" height="315" src="http://www.youtube.com/embed/B3an6Cnxs4A" frameborder="0" allowfullscreen></iframe>
-
-<iframe width="560" height="315" src="http://www.youtube.com/embed/J1A4PUZfVg8" frameborder="0" allowfullscreen></iframe>
-
-
-
-
-
-	<h1>IPO Preview</h1>
-		</td>
-		<td width='200px' valign="top" bgcolor="#e8e9ea">
-		<h1>Track Record</h1>
-<div>TECHNOLOGY PLAY
-(1) One of our highest scored stocks of 2011 was InvenSense (INVN)
-
-INVN priced at $7.50 November 15 and is now up 93% to $14.51 in about two months.
-http://finance.yahoo.com/q?s=INVN
-
-up93% in about two months
-
-It traded in the immediate aftermarket at$8.25, and is up 76% in the aftermarket.
-
-INVN pre-IPOreport
-http://gaskinsco.com/linkto-invn.shtml
-
-TWO HIGH YIELD PLAYS
-
-(2) Another favorite IPO from 2011 is Chesapeake Granite Wash Trust (CHKR) which IPO.d November 10, 2011 at $19 traded down a little and today is $23.22, up 22%.
-
-And at $19 CHKR was projected pay a yield in 2012 of 13%
-
-CHKR pre-IPOreport
-http://gaskinsco.com/linkto-chkr.shtml
-
-
-(3) We always liked CVR Partners (UAN), because of it yield and its business.  UAN IPO.d April 7, 2011 at $16, and now trades up 75% at $27.98.
-http://finance.yahoo.com/q?s=uan&ql=1
-
-UAN pre-IPOreport
-http://gaskinsco.com/linkto-uan.shtml
-
-At the pre-IPO price range mid-point of $13, UAN was expected to yield 14.8%.</div>
-		<h1>In The News</h1>
-<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis aliquet tortor, eu posuere tellus eleifend at. Quisque sed justo velit, a congue justo. Sed commodo turpis id metus ultricies ut egestas nisl ultrices. Ut urna turpis, pretium tincidunt condimentum ac, facilisis vel enim. In placerat dolor ac felis molestie hendrerit. Curabitur non urna eros. Cras tortor velit, feugiat hendrerit euismod ornare, iaculis id mauris. Vivamus placerat, lacus vel condimentum auctor, arcu nulla porttitor turpis, sed suscipit nisi est quis arcu. Donec commodo sollicitudin massa, ac mattis sapien dictum quis. Fusce felis metus, pretium eu luctus sit amet, pellentesque eu justo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi in mi et enim pharetra auctor eget vel libero. Pellentesque blandit, turpis a facilisis dignissim, justo urna porttitor metus, sit amet mollis libero ipsum faucibus odio. Sed felis turpis, gravida ac venenatis vel, vestibulum vitae neque. Duis id augue quis mi egestas suscipit sed eget ipsum. Phasellus vehicula tincidunt facilisis. </div>
-		</td>
-		<td width='200px' class="violin">&nbsp</td>
-	</tr>
-</table>		
-
-<center><small>&copy; 2012 Fiddlers Way | Page rendered in <strong>{elapsed_time}</strong> seconds</small></center>
+	<center>
+		<small>&copy; 2012 Fiddlers Way | Page rendered in <strong>{elapsed_time}</strong>
+			seconds</small>
+	</center>
 
 </body>
 </html>
