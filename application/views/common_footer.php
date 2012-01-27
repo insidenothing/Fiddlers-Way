bef9870faff08224ff29e61d4d7f92d2d1f67704
Patrick McGuire <insidenothing@gmail.com>
2012-01-27 15:43:03 -0500

    keep news

--- application/views/common_footer.php
+++ application/views/common_footer.php
@@ -1,4 +1,51 @@
-</td>
+			</td>
+			<td width='200px' valign="top" bgcolor="#e8e9ea">
+				<h1>Track Record</h1>
+				<div class="title">TECHNOLOGY PLAY</div>
+				<div class="post">
+				 <p>(1) One of our highest scored stocks of 2011 was InvenSense (INVN) INVN priced at $7.50 November
+					15 and is now up 93% to $14.51 in about two months.	http://finance.yahoo.com/q?s=INVN up93% in about two months It
+					traded in the immediate aftermarket at$8.25, and is up 76% in the
+					aftermarket. INVN pre-IPOreport</p>
+					<small>http://gaskinsco.com/linkto-invn.shtml</small> 
+				</div>
+					
+				<div class="title">TWO HIGH YIELD PLAYS</div>
+				<div class="post">
+					<p>(2) Another favorite IPO from 2011 is Chesapeake Granite Wash Trust
+					(CHKR) which IPO.d November 10, 2011 at $19 traded down a little
+					and today is $23.22, up 22%. And at $19 CHKR was projected pay a
+					yield in 2012 of 13% CHKR pre-IPOreport</p>
+					<small>http://gaskinsco.com/linkto-chkr.shtml</small> 
+					<p>(3) We always liked CVR
+					Partners (UAN), because of it yield and its business. UAN IPO.d
+					April 7, 2011 at $16, and now trades up 75% at $27.98.
+					http://finance.yahoo.com/q?s=uan&ql=1 UAN pre-IPOreport
+					At the pre-IPO price range
+					mid-point of $13, UAN was expected to yield 14.8%.</p>
+					<small>http://gaskinsco.com/linkto-uan.shtml</small>
+					</div>
+				<h1>In The News</h1>
+				<div class="post">Lorem ipsum dolor sit amet, consectetur adipiscing
+					elit. Donec lobortis aliquet tortor, eu posuere tellus eleifend at.
+					Quisque sed justo velit, a congue justo. Sed commodo turpis id
+					metus ultricies ut egestas nisl ultrices. Ut urna turpis, pretium
+					tincidunt condimentum ac, facilisis vel enim. In placerat dolor ac
+					felis molestie hendrerit. Curabitur non urna eros. Cras tortor
+					velit, feugiat hendrerit euismod ornare, iaculis id mauris. Vivamus
+					placerat, lacus vel condimentum auctor, arcu nulla porttitor
+					turpis, sed suscipit nisi est quis arcu. Donec commodo sollicitudin
+					massa, ac mattis sapien dictum quis. Fusce felis metus, pretium eu
+					luctus sit amet, pellentesque eu justo. Cum sociis natoque
+					penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+					Morbi in mi et enim pharetra auctor eget vel libero. Pellentesque
+					blandit, turpis a facilisis dignissim, justo urna porttitor metus,
+					sit amet mollis libero ipsum faucibus odio. Sed felis turpis,
+					gravida ac venenatis vel, vestibulum vitae neque. Duis id augue
+					quis mi egestas suscipit sed eget ipsum. Phasellus vehicula
+					tincidunt facilisis.</div>
+			
+			</td>
 			<td width='200px' class="violin">&nbsp</td>
 		</tr>
 	</table>
