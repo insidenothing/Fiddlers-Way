e70e660e8f6eeb768d78236092014e908cd905a5
Patrick McGuire <insidenothing@gmail.com>
2012-01-27 15:48:52 -0500

    shorten home and link to preview controller

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -25,7 +25,7 @@
 				</center>
 
 
-				<h1>IPO Preview</h1>
+				<h1>IPO Previews</h1>
 
 				<div class="title">Is the Renewable Industry Renewable?</div>
 				<div class="post">Based in Ames, Iowa, Renewable Energy Group,
@@ -33,10 +33,4 @@
 					$100 million IPO with a market capitalization of $401 million at a
 					price range mid-point of $14 for today, Thursday, January 19, 2011.
 					REGI priced last night (January 18) at $10, 28% below mid-range &
-					now trades flat. http://finance.yahoo.com/q?s=regi&ql=1 The stock
-					opened on the Nasdaq at $10.10, 1% above its target offering of
-					$10.00. A total of 7.2 million shares were sold, at prices well
-					below the expected mid-point range of $13-$15 per share. REGI's
-					outlook depends on politics, because - in our opinion - biodiesel
-					without mandates and subsidies is not a viable market. So we are
-					neutral on REGI at $10 today.</div>
+					now trades flat... <a href="/preview">[READ MORE]</a></div>
