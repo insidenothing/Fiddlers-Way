27f2df0187ea290829d7a0a3ea89cfbd5fed03d6
Patrick McGuire <insidenothing@gmail.com>
2012-02-13 17:31:46 -0500

    tracking

--- application/views/v2_header.php
+++ application/views/v2_header.php
@@ -71,7 +71,20 @@
 <script type='text/javascript'
 	src='/assest/js/et_shortcodes_frontend.js?ver=2.0'></script>
 
+<script type="text/javascript">
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-27757027-1']);
+  _gaq.push(['_setDomainName', 'fiddlersway.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 
 </head>
 <body class="home blog custom-background gecko">
