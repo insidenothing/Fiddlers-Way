76b71f1daa1ed520073836761df5853ff0cdce69
Patrick McGuire <insidenothing@gmail.com>
2012-02-13 16:05:54 -0500

    style

--- application/views/v2.php
+++ application/views/v2.php
@@ -738,10 +738,7 @@ body.custom-background {
 							</center>
 
 
-							<br />
-
-
-							<div class="block">
+							<div class="block" style="padding-top:5px;">
 								<div class="block-border">
 									<div class="block-content">
 										<div class="widget widget_text" id="text-2">"Definitely watch
