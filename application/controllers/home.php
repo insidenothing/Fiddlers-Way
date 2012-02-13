bfd7ed1e490592ff64d8aa235f175cd78a9f47f0
Patrick McGuire <insidenothing@gmail.com>
2012-02-13 15:14:08 -0500

    testing

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,9 +4,11 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('common_header');
+		/*$this->load->view('common_header');
 		$this->load->view('home_view');
 		$this->load->view('common_footer');
+		*/
+		$this->load->view('v2');
 	}
 }
 
