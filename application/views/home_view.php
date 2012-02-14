a5aaa315027d521f9ef54d2d968a69224ce7a956
Patrick McGuire <insidenothing@gmail.com>
2012-02-14 08:10:31 -0500

    new video

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,41 +1,28 @@
 
 
-	<div id="featured">
+<div class="block" style="margin-top: 5px;">
+	<div class="block-border">
+		<div class="block-content">
 
-		<div id="latest-video">
-
-			<center>
-				<iframe width="450" height="300"
-					src="http://www.youtube.com/embed/KsgGB9v5CTs" frameborder="0"
-					allowfullscreen></iframe>
-			</center>
+			<div class="widget widget_text" id="text-2">
 
 
-			<div class="block" style="margin-top: 5px;">
-				<div class="block-border">
-					<div class="block-content">
-						<div class="widget widget_text" id="text-2">"Definitely watch
-							it... but in the near term, I don't know how the stock is going
-							to act... it may not hold up that well in this market.."</div>
-					</div>
-				</div>
+<object id="cnbcplayer" height="380" width="400" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" > <param name="type" value="application/x-shockwave-flash"/> <param name="allowfullscreen" value="true"/> <param name="allowscriptaccess" value="always"/> <param name="quality" value="best"/> <param name="scale" value="noscale" /> <param name="wmode" value="transparent"/> <param name="bgcolor" value="#000000"/> <param name="salign" value="lt"/> <param name="flashVars" value="startTime=000"/> <param name="flashVars" value="endTime=000"/> <param name="movie" value="http://plus.cnbc.com/rssvideosearch/action/player/id/3000070318/code/cnbcplayershare" /> <embed name="cnbcplayer" PLUGINSPAGE="http://www.macromedia.com/go/getflashplayer" allowfullscreen="true" allowscriptaccess="always" bgcolor="#000000" height="380" width="400" quality="best" wmode="transparent" scale="noscale" salign="lt" src="http://plus.cnbc.com/rssvideosearch/action/player/id/3000070318/code/cnbcplayershare" type="application/x-shockwave-flash" /></object>
+
+
+				<br> Francis Gaskins on CNBC analyzing facebook
 			</div>
-
-
-
-
-
-
-
-
 		</div>
-		<!-- end #slides -->
-
-
-
 	</div>
-	<!-- end #featured -->
+</div>
 
+
+
+
+
+
+
+	
 
 	<div class="entry">
 		<div class="border">
