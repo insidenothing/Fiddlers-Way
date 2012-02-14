f3bb8d20aec8cff576907d0d3d21252fd47ed985
Patrick McGuire <insidenothing@gmail.com>
2012-02-13 20:55:19 -0500

    okie

--- application/controllers/page.php
+++ application/controllers/page.php
@@ -4,8 +4,12 @@ class Page extends CI_Controller {
 
 	public function index()
 	{
+		
+		$data['title'] 		= "Page Title";
+		$data['content'] 	= "Page Content";
+		
 		$this->load->view('common_header');
-		$this->load->view('page_view');
+		$this->load->view('page_view',$data);
 		$this->load->view('common_footer');
 	}
 }
--- application/views/page_view.php
+++ application/views/page_view.php
@@ -1,10 +1,8 @@
-
-
-
-			<div class="block" style="margin-top: 5px;">
-				<div class="block-border">
-					<div class="block-content">
-						<div class="widget widget_text" id="text-2">page content</div>
-					</div>
-				</div>
-			</div>
+<div class="block" style="margin-top: 5px;">
+	<div class="block-border">
+		<div class="block-content">
+			<h4 class="widgettitle"><?php echo $title;?></h4>
+			<div class="widget widget_text" id="text-2"><?php echo $content;?></div>
+		</div>
+	</div>
+</div>
