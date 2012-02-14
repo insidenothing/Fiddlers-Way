8ae9d9829be62445dd696a4e6909fadd3dd7a193
Patrick McGuire <insidenothing@gmail.com>
2012-02-13 21:12:45 -0500

    darker

--- application/views/video_view.php
+++ application/views/video_view.php
@@ -4,17 +4,17 @@
 <div class="block" style="margin-top: 5px;">
 	<div class="block-border">
 		<div class="block-content">
-			
+
 			<div class="widget widget_text" id="text-2">
-			
-			<iframe width="400" height="300"
-		src="http://www.youtube.com/embed/KsgGB9v5CTs" frameborder="0"
-		allowfullscreen></iframe>
-			
+
+				<iframe width="400" height="300"
+					src="http://www.youtube.com/embed/KsgGB9v5CTs" frameborder="0"
+					allowfullscreen></iframe>
+
+				<br> "Definitely watch it... but in the near term, I don't know how
+				the stock is going to act... it may not hold up that well in this
+				market.."
 			</div>
-			<h4 class="widgettitle">"Definitely watch it... but in the near term, I
-	don't know how the stock is going to act... it may not hold up that
-	well in this market.."</h4>
 		</div>
 	</div>
 </div>
@@ -25,10 +25,12 @@
 <div class="block" style="margin-top: 5px;">
 	<div class="block-border">
 		<div class="block-content">
-			
-			<div class="widget widget_text" id="text-2"><iframe width="400" height="300"
-		src="http://www.youtube.com/embed/B3an6Cnxs4A" frameborder="0"
-		allowfullscreen></iframe></div>
+
+			<div class="widget widget_text" id="text-2">
+				<iframe width="400" height="300"
+					src="http://www.youtube.com/embed/B3an6Cnxs4A" frameborder="0"
+					allowfullscreen></iframe>
+			</div>
 		</div>
 	</div>
 </div>
@@ -36,10 +38,12 @@
 <div class="block" style="margin-top: 5px;">
 	<div class="block-border">
 		<div class="block-content">
-			
-			<div class="widget widget_text" id="text-2"><iframe width="400" height="300"
-		src="http://www.youtube.com/embed/J1A4PUZfVg8" frameborder="0"
-		allowfullscreen></iframe></div>
+
+			<div class="widget widget_text" id="text-2">
+				<iframe width="400" height="300"
+					src="http://www.youtube.com/embed/J1A4PUZfVg8" frameborder="0"
+					allowfullscreen></iframe>
+			</div>
 		</div>
 	</div>
 </div>
