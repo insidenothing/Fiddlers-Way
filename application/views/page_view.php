4c830d024ff38ba0e250d9b4671a491193a8e678
Patrick McGuire <insidenothing@gmail.com>
2012-02-13 20:47:14 -0500

    ok

--- application/views/page_view.php
+++ application/views/page_view.php
@@ -1,1 +1,10 @@
-blog view
+
+
+
+			<div class="block" style="margin-top: 5px;">
+				<div class="block-border">
+					<div class="block-content">
+						<div class="widget widget_text" id="text-2">page content</div>
+					</div>
+				</div>
+			</div>
