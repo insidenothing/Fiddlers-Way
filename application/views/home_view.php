f51ea8c03022e69dfc2176e8c2cc73c75ee46f2a
Patrick McGuire <insidenothing@gmail.com>
2012-02-14 08:15:28 -0500

    center video

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,212 +1,224 @@
-
-
 <div class="block" style="margin-top: 5px;">
 	<div class="block-border">
 		<div class="block-content">
-
 			<div class="widget widget_text" id="text-2">
+				<center>
+					<object id="cnbcplayer" height="380" width="400"
+						classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
+						codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0">
+						<param name="type" value="application/x-shockwave-flash" />
+						<param name="allowfullscreen" value="true" />
+						<param name="allowscriptaccess" value="always" />
+						<param name="quality" value="best" />
+						<param name="scale" value="noscale" />
+						<param name="wmode" value="transparent" />
+						<param name="bgcolor" value="#000000" />
+						<param name="salign" value="lt" />
+						<param name="flashVars" value="startTime=000" />
+						<param name="flashVars" value="endTime=000" />
+						<param name="movie"
+							value="http://plus.cnbc.com/rssvideosearch/action/player/id/3000070318/code/cnbcplayershare" />
+						<embed name="cnbcplayer"
+							PLUGINSPAGE="http://www.macromedia.com/go/getflashplayer"
+							allowfullscreen="true" allowscriptaccess="always"
+							bgcolor="#000000" height="380" width="400" quality="best"
+							wmode="transparent" scale="noscale" salign="lt"
+							src="http://plus.cnbc.com/rssvideosearch/action/player/id/3000070318/code/cnbcplayershare"
+							type="application/x-shockwave-flash" />
+					</object>
 
-
-<object id="cnbcplayer" height="380" width="400" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" > <param name="type" value="application/x-shockwave-flash"/> <param name="allowfullscreen" value="true"/> <param name="allowscriptaccess" value="always"/> <param name="quality" value="best"/> <param name="scale" value="noscale" /> <param name="wmode" value="transparent"/> <param name="bgcolor" value="#000000"/> <param name="salign" value="lt"/> <param name="flashVars" value="startTime=000"/> <param name="flashVars" value="endTime=000"/> <param name="movie" value="http://plus.cnbc.com/rssvideosearch/action/player/id/3000070318/code/cnbcplayershare" /> <embed name="cnbcplayer" PLUGINSPAGE="http://www.macromedia.com/go/getflashplayer" allowfullscreen="true" allowscriptaccess="always" bgcolor="#000000" height="380" width="400" quality="best" wmode="transparent" scale="noscale" salign="lt" src="http://plus.cnbc.com/rssvideosearch/action/player/id/3000070318/code/cnbcplayershare" type="application/x-shockwave-flash" /></object>
-
-
-				<br> Francis Gaskins on CNBC analyzing facebook
+					<br> Francis Gaskins on CNBC analyzing facebook
+				</center>
 			</div>
 		</div>
 	</div>
 </div>
 
 
+<div class="entry">
+	<div class="border">
+		<div class="bottom">
+			<div class="entry-content clearfix">
+				<h3 class="title">
+					<a href="http://www.fiddlersway.com/archives/53">Fiddler&#8217;s
+						Way</a>
+				</h3>
+				<p class="meta-info">
+					Feb 9, 2012 <span>by</span> <a
+						href="http://www.fiddlersway.com/archives/author/admin"
+						title="Posts by Francis and Doug" rel="author">Francis and Doug</a>
+				</p>
 
-
-
-
-
-	
-
-	<div class="entry">
-		<div class="border">
-			<div class="bottom">
-				<div class="entry-content clearfix">
-					<h3 class="title">
-						<a href="http://www.fiddlersway.com/archives/53">Fiddler&#8217;s
-							Way</a>
-					</h3>
-
-					<p class="meta-info">
-						Feb 9, 2012 <span>by</span> <a
-							href="http://www.fiddlersway.com/archives/author/admin"
-							title="Posts by Francis and Doug" rel="author">Francis and Doug</a>
-					</p>
-
-					<p>&nbsp; &nbsp; Bookmark on Delicious Digg this post Recommend on
-						Facebook share via Reddit Share with Stumblers Tweet about it...</p>
-					<a href="http://www.fiddlersway.com/archives/53" class="readmore"><span>read
-							more</span> </a>
-				</div>
-				<!-- end .entry-content -->
+				<p>&nbsp; &nbsp; Bookmark on Delicious Digg this post Recommend on
+					Facebook share via Reddit Share with Stumblers Tweet about it...</p>
+				<a href="http://www.fiddlersway.com/archives/53" class="readmore"><span>read
+						more</span> </a>
 			</div>
-			<!-- end .bottom -->
+			<!-- end .entry-content -->
 		</div>
-		<!-- end .border -->
+		<!-- end .bottom -->
 	</div>
-	<!-- end .entry -->
+	<!-- end .border -->
+</div>
+<!-- end .entry -->
 
 
-	<div class="entry last">
-		<div class="border">
-			<div class="bottom">
-				<div class="entry-content clearfix">
-					<h3 class="title">
-						<a href="http://www.fiddlersway.com/archives/40">Francis Gaskins
-							in the News - ...</a>
-					</h3>
-					<p class="meta-info">
-						Feb 3, 2012 <span>by</span> <a
-							href="http://www.fiddlersway.com/archives/author/admin"
-							title="Posts by Francis and Doug" rel="author">Francis and Doug</a>
-					</p>
+<div class="entry last">
+	<div class="border">
+		<div class="bottom">
+			<div class="entry-content clearfix">
+				<h3 class="title">
+					<a href="http://www.fiddlersway.com/archives/40">Francis Gaskins in
+						the News - ...</a>
+				</h3>
+				<p class="meta-info">
+					Feb 3, 2012 <span>by</span> <a
+						href="http://www.fiddlersway.com/archives/author/admin"
+						title="Posts by Francis and Doug" rel="author">Francis and Doug</a>
+				</p>
 
 
-					<p>What is Facebook worth? It will be months before the market sets
-						a final price, but already the valuation question has become a tug
-						of...</p>
-					<a href="http://www.fiddlersway.com/archives/40" class="readmore"><span>read
-							more</span> </a>
-				</div>
-				<!-- end .entry-content -->
+				<p>What is Facebook worth? It will be months before the market sets
+					a final price, but already the valuation question has become a tug
+					of...</p>
+				<a href="http://www.fiddlersway.com/archives/40" class="readmore"><span>read
+						more</span> </a>
 			</div>
-			<!-- end .bottom -->
+			<!-- end .entry-content -->
 		</div>
-		<!-- end .border -->
+		<!-- end .bottom -->
 	</div>
-	<!-- end .entry -->
+	<!-- end .border -->
+</div>
+<!-- end .entry -->
 
-	<div class="entry">
+<div class="entry">
 
-		<div class="border">
-			<div class="bottom">
-				<div class="entry-content clearfix">
-					<h3 class="title">
-						<a href="http://www.fiddlersway.com/archives/36">Gaskins on
-							Facebook IPO on Al ...</a>
-					</h3>
-					<p class="meta-info">
-						Feb 3, 2012 <span>by</span> <a
-							href="http://www.fiddlersway.com/archives/author/admin"
-							title="Posts by Francis and Doug" rel="author">Francis and Doug</a>
-					</p>
+	<div class="border">
+		<div class="bottom">
+			<div class="entry-content clearfix">
+				<h3 class="title">
+					<a href="http://www.fiddlersway.com/archives/36">Gaskins on
+						Facebook IPO on Al ...</a>
+				</h3>
+				<p class="meta-info">
+					Feb 3, 2012 <span>by</span> <a
+						href="http://www.fiddlersway.com/archives/author/admin"
+						title="Posts by Francis and Doug" rel="author">Francis and Doug</a>
+				</p>
 
-					<p>Francis Gaskins "bookends" this interview, appearing at the
-						beginning and at the end and providing insights into the
-						forthcoming...</p>
+				<p>Francis Gaskins "bookends" this interview, appearing at the
+					beginning and at the end and providing insights into the
+					forthcoming...</p>
 
-					<a href="http://www.fiddlersway.com/archives/36" class="readmore"><span>read
-							more</span> </a>
-				</div>
-				<!-- end .entry-content -->
+				<a href="http://www.fiddlersway.com/archives/36" class="readmore"><span>read
+						more</span> </a>
 			</div>
-			<!-- end .bottom -->
+			<!-- end .entry-content -->
 		</div>
-		<!-- end .border -->
+		<!-- end .bottom -->
 	</div>
-	<!-- end .entry -->
+	<!-- end .border -->
+</div>
+<!-- end .entry -->
 
-	<div class="entry last">
-		<div class="border">
-			<div class="bottom">
+<div class="entry last">
+	<div class="border">
+		<div class="bottom">
 
-				<div class="entry-content clearfix">
-					<h3 class="title">
-						<a href="http://www.fiddlersway.com/archives/32">Renewable Energy
-							Group, Inc. -...</a>
-					</h3>
-					<p class="meta-info">
-						Feb 3, 2012 <span>by</span> <a
-							href="http://www.fiddlersway.com/archives/author/admin"
-							title="Posts by Francis and Doug" rel="author">Francis and Doug</a>
-					</p>
+			<div class="entry-content clearfix">
+				<h3 class="title">
+					<a href="http://www.fiddlersway.com/archives/32">Renewable Energy
+						Group, Inc. -...</a>
+				</h3>
+				<p class="meta-info">
+					Feb 3, 2012 <span>by</span> <a
+						href="http://www.fiddlersway.com/archives/author/admin"
+						title="Posts by Francis and Doug" rel="author">Francis and Doug</a>
+				</p>
 
-					<p>When it first went public last month, we noted that it was
-						opening well below its target, and indicated that this company's
-						market...</p>
-					<a href="http://www.fiddlersway.com/archives/32" class="readmore"><span>read
-							more</span> </a>
+				<p>When it first went public last month, we noted that it was
+					opening well below its target, and indicated that this company's
+					market...</p>
+				<a href="http://www.fiddlersway.com/archives/32" class="readmore"><span>read
+						more</span> </a>
 
-				</div>
-				<!-- end .entry-content -->
 			</div>
-			<!-- end .bottom -->
+			<!-- end .entry-content -->
 		</div>
-		<!-- end .border -->
+		<!-- end .bottom -->
 	</div>
-	<!-- end .entry -->
+	<!-- end .border -->
+</div>
+<!-- end .entry -->
 
-	<div class="entry">
-		<div class="border">
-			<div class="bottom">
-				<div class="entry-content clearfix">
+<div class="entry">
+	<div class="border">
+		<div class="bottom">
+			<div class="entry-content clearfix">
 
-					<h3 class="title">
-						<a href="http://www.fiddlersway.com/archives/30">Facebooks 100
-							Billion Too High...</a>
-					</h3>
-					<p class="meta-info">
-						Feb 3, 2012 <span>by</span> <a
-							href="http://www.fiddlersway.com/archives/author/admin"
-							title="Posts by Francis and Doug" rel="author">Francis and Doug</a>
-					</p>
+				<h3 class="title">
+					<a href="http://www.fiddlersway.com/archives/30">Facebooks 100
+						Billion Too High...</a>
+				</h3>
+				<p class="meta-info">
+					Feb 3, 2012 <span>by</span> <a
+						href="http://www.fiddlersway.com/archives/author/admin"
+						title="Posts by Francis and Doug" rel="author">Francis and Doug</a>
+				</p>
 
-					<p>Bookmark on Delicious Digg this post Recommend on Facebook share
-						via Reddit Share with Stumblers Tweet about it Subscribe to the
-						comme</p>
-					<a href="http://www.fiddlersway.com/archives/30" class="readmore"><span>read
-							more</span> </a>
-				</div>
-				<!-- end .entry-content -->
+				<p>Bookmark on Delicious Digg this post Recommend on Facebook share
+					via Reddit Share with Stumblers Tweet about it Subscribe to the
+					comme</p>
+				<a href="http://www.fiddlersway.com/archives/30" class="readmore"><span>read
+						more</span> </a>
 			</div>
-			<!-- end .bottom -->
+			<!-- end .entry-content -->
 		</div>
-		<!-- end .border -->
+		<!-- end .bottom -->
 	</div>
-	<!-- end .entry -->
+	<!-- end .border -->
+</div>
+<!-- end .entry -->
 
 
-	<div class="entry last">
-		<div class="border">
-			<div class="bottom">
-				<div class="entry-content clearfix">
-					<h3 class="title">
-						<a href="http://www.fiddlersway.com/archives/20">The Facebook IPO
-							Basics</a>
-					</h3>
-					<p class="meta-info">
-						Feb 1, 2012 <span>by</span> <a
-							href="http://www.fiddlersway.com/archives/author/admin"
-							title="Posts by Francis and Doug" rel="author">Francis and Doug</a>
-					</p>
+<div class="entry last">
+	<div class="border">
+		<div class="bottom">
+			<div class="entry-content clearfix">
+				<h3 class="title">
+					<a href="http://www.fiddlersway.com/archives/20">The Facebook IPO
+						Basics</a>
+				</h3>
+				<p class="meta-info">
+					Feb 1, 2012 <span>by</span> <a
+						href="http://www.fiddlersway.com/archives/author/admin"
+						title="Posts by Francis and Doug" rel="author">Francis and Doug</a>
+				</p>
 
 
-					<p>Summary/Overview The pending Facebook Initial Public Offering -
-						IPO - will put, at least for the moment, put IPOs on the map,...</p>
-					<a href="http://www.fiddlersway.com/archives/20" class="readmore"><span>read
-							more</span> </a>
-				</div>
-				<!-- end .entry-content -->
+				<p>Summary/Overview The pending Facebook Initial Public Offering -
+					IPO - will put, at least for the moment, put IPOs on the map,...</p>
+				<a href="http://www.fiddlersway.com/archives/20" class="readmore"><span>read
+						more</span> </a>
 			</div>
-			<!-- end .bottom -->
+			<!-- end .entry-content -->
 		</div>
-		<!-- end .border -->
+		<!-- end .bottom -->
 	</div>
-	<!-- end .entry -->
+	<!-- end .border -->
+</div>
+<!-- end .entry -->
 
-	<div class="clear"></div>
+<div class="clear"></div>
 
-	<div id="controllers">
-		<a href="http://www.fiddlersway.com/page/2"><span id="right-arrow"></span>
-		</a>
-	</div>
-	<!-- end #controllers -->
+<div id="controllers">
+	<a href="http://www.fiddlersway.com/page/2"><span id="right-arrow"></span>
+	</a>
+</div>
+<!-- end #controllers -->
+
+
 
 
 
