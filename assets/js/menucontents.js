27faf6219eb2352c9b5dfb937d35574144031801
Patrick McGuire <insidenothing@gmail.com>
2012-02-14 11:37:53 -0500

    case

--- assets/js/menucontents.js
+++ assets/js/menucontents.js
@@ -1,7 +1,7 @@
 var anylinkmenu1={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
 anylinkmenu1.items=[
-	["BACKGROUNDER", "http://www.fiddlersway.com/page/index/backgrounder"],
-	["BIO", "http://www.fiddlersway.com/page/index/bio"]//no comma following last entry!
+	["Backgrounder", "http://www.fiddlersway.com/page/index/backgrounder"],
+	["Bio", "http://www.fiddlersway.com/page/index/bio"]//no comma following last entry!
 ]
 
 
@@ -9,24 +9,24 @@ anylinkmenu1.items=[
 
 var anylinkmenu2={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
 anylinkmenu2.items=[
-	["NEW FEATURING US", "http://www.fiddlersway.com/page/index/new-featuring-us"],
-	["VIDEO NEWS FEATURING US", "http://www.fiddlersway.com/page/index/video-news-featuring-us"],
-	["IPO NEWS", "http://www.fiddlersway.com/page/index/ipo-news"]
+	["New Featuring Us", "http://www.fiddlersway.com/page/index/new-featuring-us"],
+	["Video News Featuring Us", "http://www.fiddlersway.com/page/index/video-news-featuring-us"],
+	["IPO News", "http://www.fiddlersway.com/page/index/ipo-news"]
 ]
 
 
 var anylinkmenu3={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
 anylinkmenu3.items=[
-	["NEWS VIDEOS", "http://www.fiddlersway.com/page/index/new-featuring-us"],
-	["VIDEO BLOGS", "http://www.fiddlersway.com/page/index/video-news-featuring-us"],
-	["VIDEO NEWS", "http://www.fiddlersway.com/page/index/ipo-news-other-sources"]
+	["News Videos", "http://www.fiddlersway.com/page/index/new-featuring-us"],
+	["Video Blogs", "http://www.fiddlersway.com/page/index/video-news-featuring-us"],
+	["Video News", "http://www.fiddlersway.com/page/index/ipo-news-other-sources"]
 ]
 
 var anylinkmenu4={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
 anylinkmenu4.items=[
-	["BLOGS", "http://www.fiddlersway.com/page/index/new-featuring-us"],
-	["WHITE PAPERS", "http://www.fiddlersway.com/page/index/video-news-featuring-us"],
-	["CASE STUDIES", "http://www.fiddlersway.com/page/index/ipo-news-other-sources"]
+	["Blogs", "http://www.fiddlersway.com/page/index/new-featuring-us"],
+	["White Papers", "http://www.fiddlersway.com/page/index/video-news-featuring-us"],
+	["Case Studies", "http://www.fiddlersway.com/page/index/ipo-news-other-sources"]
 ]
 
 
@@ -34,7 +34,7 @@ var anylinkmenu5={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First
 anylinkmenu5.items=[
 	["IPO BUY RECS", "http://www.fiddlersway.com/page/index/new-featuring-us"],
 	["IPO SELL RECS", "http://www.fiddlersway.com/page/index/video-news-featuring-us"],
-	["PROJECTION BLOGS", "http://www.fiddlersway.com/page/index/ipo-news-other-sources"]
+	["Projection Blogs", "http://www.fiddlersway.com/page/index/ipo-news-other-sources"]
 ]
 
 
