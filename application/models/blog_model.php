645207c359f037d5eb3f24197be47cae7c5aa4a1
Patrick McGuire <insidenothing@gmail.com>
2012-02-15 16:21:53 -0500

    cleaner

--- application/models/blog_model.php
+++ application/models/blog_model.php
@@ -23,6 +23,36 @@ class Page_model extends CI_Model {
 		return $row->title;
 	}
 
+	function get_author($id)
+	{
+		if ($id > 0)
+		{
+			$query = $this->db->query("SELECT author from pages where id = '$id'");
+		}else
+		{
+			$limit = ($id*-1)-1;
+			$query = $this->db->query("SELECT author from pages order by id DESC limit $limit,1");
+		}
+		$row = $query->row();
+		$query->free_result();
+		return $row->author;
+	}
+
+	function get_published($id)
+	{
+		if ($id > 0)
+		{
+			$query = $this->db->query("SELECT published from pages where id = '$id'");
+		}else
+		{
+			$limit = ($id*-1)-1; 
+			$query = $this->db->query("SELECT published from pages order by id DESC limit $limit,1");
+		}
+		$row = $query->row();
+		$query->free_result();
+		return $row->published;
+	}
+
 	function get_contents($id)
 	{
 			if ($id > 0)
--- application/views/blog_view.php
+++ application/views/blog_view.php
@@ -2,7 +2,9 @@
 	<div class="block-border">
 		<div class="block-content">
 			<h4 class="widgettitle"><?php echo $title;?></h4>
-			<div class="widget widget_text" id="text-2"><?php echo $content;?></div>
+			<div class="author">Written by <?php echo $author;?></div>
+			<div class="date"><?php echo $published;?></div>
+			<div class="widget widget_text" id="text-2"><?php echo $contents;?></div>
 		</div>
 	</div>
 </div>
