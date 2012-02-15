d8fe28648ebe09b4df4cf9a275f53a32040ddae0
Patrick McGuire <insidenothing@gmail.com>
2012-02-15 16:32:09 -0500

    fix

--- application/controllers/page.php
+++ application/controllers/page.php
@@ -4,21 +4,9 @@ class Page extends CI_Controller {
 
 	public function index($id='1')
 	{
-		
-		
 		$this->load->model('page_model','page');
 		$data['title'] = $this->page->get_title($id);
-		$data['author'] = $this->page->get_author($id);
-		$data['published'] = $this->page->get_published($id);
 		$data['contents'] = $this->page->get_contents($id);
-		
-		
-		
-		
-		
-		
-		
-		
 		$this->load->library('Menu','menu');
 		$this->menu->load_common('page_view',$data);
 	}
