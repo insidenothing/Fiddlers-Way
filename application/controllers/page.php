234fed45db13d9270a71dd0d2f5b80777786fdf1
Patrick McGuire <insidenothing@gmail.com>
2012-02-15 16:17:44 -0500

    fix

--- application/controllers/page.php
+++ application/controllers/page.php
@@ -2,11 +2,11 @@
 
 class Page extends CI_Controller {
 
-	public function index($link='')
+	public function index($id='')
 	{
 		
 		
-		$this->load->model('page_model','blog');
+		$this->load->model('page_model','page');
 		$data['title'] = $this->page->get_title($id);
 		$data['author'] = $this->page->get_author($id);
 		$data['published'] = $this->page->get_published($id);
