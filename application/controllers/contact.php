89dd137e4d0246560845630e82e789f7aa435671
Patrick McGuire <insidenothing@gmail.com>
2012-02-15 15:47:59 -0500

    better

--- application/controllers/contact.php
+++ application/controllers/contact.php
@@ -10,16 +10,9 @@ class Contact extends CI_Controller {
 		/** Newsletter Status */
 		if ($this->input->cookie('email')){
 			/* Logged In User */
-			$data['newsletter_status'] = $this->contact->check_user($this->input->cookie('email'));
 			$data['email'] = $this->input->cookie('email');		
-		}elseif($this->input->post('email'))
 		{
-			$data['newsletter_status'] = $this->contact->check_user($this->input->post('email'));
-			$data['email'] = $this->input->post('email');
-		} else 
-		{
-			$data['newsletter_status'] = '';
-			$data['email'] = '';
+			$data['email'] = '<input name="email" value="Enter E-Mail Address" />';
 		}
 		
 		
