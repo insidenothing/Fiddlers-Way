b008fe958cb5c5a0a047ef14baaf36ad2deae4b9
Patrick McGuire <insidenothing@gmail.com>
2012-02-15 11:08:23 -0500

    better

--- application/views/newsletter_view.php
+++ application/views/newsletter_view.php
@@ -6,72 +6,58 @@
 					<tr>
 						<td>NAME</td>
 						<td><input name="name" value=""></td>
-						<td></td>
 					</tr>
 					<tr>
 						<td>ADDRESS</td>
 						<td><input name="address" value=""></td>
-						<td></td>
 					</tr>
 					<tr>
 						<td>PHONE NUMBER(S)</td>
 						<td><input name="phone" value=""></td>
-						<td></td>
 					</tr>
 					<tr>
-						<td>Receive Newsletter</td>
-						<td></td>
-						<td>opt-out-link</td>
+						<td>Newsletter Status</td>
+						<td>opt-out|opt-in|paid</td>
 					</tr>
 					<tr>
-						<td></td>
 						<td>INVESTMENT OBJECTIVE(S)</td>
 						<td>(describe)</td>
 					</tr>
 					<tr>
 						<td>SPECULATION</td>
 						<td><textarea name="obj_SPECULATION"></textarea></td>
-						<td></td>
 					</tr>
 					<tr>
 						<td>GROWTH</td>
 						<td><textarea name="obj_GROWTH"></textarea></td>
-						<td></td>
 					</tr>
 					<tr>
 						<td>CAPITAL PRESERVATION</td>
 						<td><textarea name="obj_CAPITAL PRESERVATION"></textarea></td>
-						<td></td>
 					</tr>
 					<tr>
 						<td>INCOME</td>
 						<td><textarea name="obj_INCOME"></textarea></td>
-						<td></td>
 					</tr>
 					<tr>
-						<td></td>
 						<td>INVESTMENT EXPERIENCE</td>
 						<td>(CHECK ALL THAT APPLY)</td>
 					</tr>
 					<tr>
 						<td>STOCK / MUTUAL FUNDS</td>
 						<td><input type="checkbox" name="exp_stock_mf"></td>
-						<td></td>
 					</tr>
 					<tr>
 						<td>BONDS</td>
 						<td><input type="checkbox" name="exp_bonds"></td>
-						<td></td>
 					</tr>
 					<tr>
 						<td>COMMODITIES / FOREX</td>
 						<td><input type="checkbox" name="exp_commodities_forex"></td>
-						<td></td>
 					</tr>
 					<tr>
 						<td>PRIVATE PLACEMENTS</td>
 						<td><input type="checkbox" name="exp_private_placement"></td>
-						<td></td>
 					</tr>
 					<tr>
 						<td></td>
@@ -81,17 +67,14 @@
 					<tr>
 						<td>AGGRESSIVE</td>
 						<td><input type="radio" name="risk_tolarance" value="Aggressive"></td>
-						<td></td>
 					</tr>
 					<tr>
 						<td>MODERATE</td>
 						<td><input type="radio" name="risk_tolarance" value="Moderate"></td>
-						<td></td>
 					</tr>
 					<tr>
 						<td>CONSERVATIVE</td>
 						<td><input type="radio" name="risk_tolarance" value="Conservative"></td>
-						<td></td>
 					</tr>
 				</table>
 				<img src="/assets/images/slogan-bg.png" width="400" height="25" />
