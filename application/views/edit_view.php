087bb8b3ddd913606efed10bab0639e38cd7c358
Patrick McGuire <insidenothing@gmail.com>
2012-02-15 18:54:06 -0500

    action

--- application/views/edit_view.php
+++ application/views/edit_view.php
@@ -1,17 +1,66 @@
 
 <script
-	language="JavaScript" type="text/javascript" src="/assets/wysiwyg/wysiwyg.js"></script>
+	language="JavaScript" type="text/javascript"
+	src="/assets/wysiwyg/wysiwyg.js"></script>
 
-<form method="POST" action="/edit/index/<?php echo $type; ?>/<?php echo $id; ?>">
+<form method="POST"
+	action="/edit/index/<?php echo $type; ?>/<?php echo $id; ?>">
+
+
+
+<div class="block" style="margin-top: 5px;">
+	<div class="block-border">
+		<div class="block-content">
+
+			<div class="widget widget_text" id="text-2">
+			<h4><?php echo $type;?> Settings #<?php echo $id;?></h4>
+			<table>
+				<tr>
+					<td>Author</td>
+					<td><input name="author"></td>
+				</tr>
+				<tr>
+					<td>Published</td>
+					<td><input name="published"></td>
+				</tr>
+				<tr>
+					<td>Title</td>
+					<td><input name="title"></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit"></td>
+				</tr>
+			</table>				
 	
-<textarea id="whiteboard" rows="30" cols="100" name="whiteboard">
+				
+			</div>
+		</div>
+	</div>
+</div>
 
 
-<?PHP echo $contents;?></textarea>
 
-<script language="JavaScript">
+	<div class="block" style="margin-top: 5px;">
+		<div class="block-border">
+			<div class="block-content">
+
+				<div class="widget widget_text" id="text-2">
+
+					<textarea id="whiteboard" rows="30" cols="100" name="whiteboard">
+
+
+
+
+					<?PHP echo $contents;?></textarea>
+
+					<script language="JavaScript">
 generate_wysiwyg('whiteboard');
 </script>
 
+				</div>
+			</div>
+		</div>
+	</div>
 
 </form>
