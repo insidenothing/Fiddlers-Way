766e320dfcd1b831f0bd3963f548dd35eea3e497
Patrick McGuire <insidenothing@gmail.com>
2012-02-15 15:56:21 -0500

    better

--- application/views/contact_view.php
+++ application/views/contact_view.php
@@ -2,11 +2,39 @@
 	<div class="block-border">
 		<div class="block-content">
 			<div class="widget widget_text">
-				<h4>Contact</h4>
+				<h4>West Palm Beach, FL Office</h4>
+				Doug McLean<br>
+				doug@fiddlersway.com<br>
+				561 866-4751<br>
+				<br>
+				<h4>Los Angeles, CA Office</h4>
+				info@fiddlersway.com<br>
+				<br>		
+				<center><img src="/assets/images/slogan-bg.png" width="400" height="25" /><br><input type="submit" value="Save"></center>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="block" style="margin-top: 5px;">
+	<div class="block-border">
+		<div class="block-content">
+			<div class="widget widget_text">
+				<h4>Send Message</h4>
 				<table width="100%">
 					<tr>
 						<td width="50%">From:</td>
 						<td width="50%"><?php echo $email;?></td>
+					</tr>
+					<tr>
+						<td width="50%">Subject:</td>
+						<td width="50%"><input name="subject"></td>
+					</tr>
+					<tr>
+						<td colspan="2"><textarea name="body"></textarea></td>
+					</tr>
+					<tr>
+						<td colspan="2"><input type="submit" value="Send"></td>
 					</tr>
 				</table>
 				<center><img src="/assets/images/slogan-bg.png" width="400" height="25" /><br><input type="submit" value="Save"></center>
@@ -14,3 +42,6 @@
 		</div>
 	</div>
 </div>
+
+
+
