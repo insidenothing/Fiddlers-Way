7423dfd18f4dfc29b27728dd967a1bc285288f33
Patrick McGuire <insidenothing@gmail.com>
2012-02-15 10:57:53 -0500

    layout

--- application/views/newsletter_view.php
+++ application/views/newsletter_view.php
@@ -1,32 +1,103 @@
-<h1>Newsletter Settings</h1>
-
-
-<?php if (isset($debug)){?>
-<p><?php echo $debug; ?></p>
-<?php }?>
-
-
-Form:
-<pre>
-NAME
-ADDRESS
-PHONE NUMBER(S)
- 
-INVESTMENT OBJECTIVE(S)
-- SPECULATION
-- GROWTH
-- CAPITAL PRESERVATION
-- INCOME
- 
-INVESTMENT EXPERIENCE (CHECK ALL THAT APPLY)
--STOCK / MUTUAL FUNDS
-- BONDS
-- COMMODITIES / FOREX
-- PRIVATE PLACEMENTS
- 
-RISK TOLERANCE
-- AGGRESSIVE
-- MODERATE
-- CONSERVATIVE
-
-</pre>
+<div class="block" style="margin-top: 5px;">
+	<div class="block-border">
+		<div class="block-content">
+			<div class="widget widget_text">
+				<table>
+					<tr>
+						<td>NAME</td>
+						<td><input name="name" value="<?php echo $name;?>"></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>ADDRESS</td>
+						<td><input name="address" value="<?php echo $address;?>"></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>PHONE NUMBER(S)</td>
+						<td><input name="phone" value="<?php echo $PHONE;?>"></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>Receive Newsletter</td>
+						<td><?php echo $newsletter_status;?></td>
+						<td>opt-out-link</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td>INVESTMENT OBJECTIVE(S)</td>
+						<td>(describe)</td>
+					</tr>
+					<tr>
+						<td>SPECULATION</td>
+						<td><textarea name="obj_SPECULATION"></textarea></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>GROWTH</td>
+						<td><textarea name="obj_GROWTH"></textarea></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>CAPITAL PRESERVATION</td>
+						<td><textarea name="obj_CAPITAL PRESERVATION"></textarea></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>INCOME</td>
+						<td><textarea name="obj_INCOME"></textarea></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td>INVESTMENT EXPERIENCE</td>
+						<td>(CHECK ALL THAT APPLY)</td>
+					</tr>
+					<tr>
+						<td>STOCK / MUTUAL FUNDS</td>
+						<td><input type="checkbox" name="exp_stock_mf"></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>BONDS</td>
+						<td><input type="checkbox" name="exp_bonds"></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>COMMODITIES / FOREX</td>
+						<td><input type="checkbox" name="exp_commodities_forex"></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>PRIVATE PLACEMENTS</td>
+						<td><input type="checkbox" name="exp_private_placement"></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td>RISK TOLERANCE</td>
+						<td>(select one)</td>
+					</tr>
+					<tr>
+						<td>AGGRESSIVE</td>
+						<td><input type="radio" name="risk_tolarance" value="Aggressive"></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>MODERATE</td>
+						<td><input type="radio" name="risk_tolarance" value="Moderate"></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>CONSERVATIVE</td>
+						<td><input type="radio" name="risk_tolarance" value="Conservative"></td>
+						<td></td>
+					</tr>
+				</table>
+				<img src="/assets/images/slogan-bg.png" width="400" height="25" /> <br>
+				Francis Gaskins on CNBC analyzing facebook
+				</center>
+			</div>
+		</div>
+	</div>
+</div>
