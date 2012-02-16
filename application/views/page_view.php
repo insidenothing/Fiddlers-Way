3f131e084f46273a31a0a525aa2a5a1bc6badbb9
Patrick McGuire <insidenothing@gmail.com>
2012-02-15 19:08:50 -0500

    better

--- application/views/page_view.php
+++ application/views/page_view.php
@@ -2,7 +2,7 @@
 	<div class="block-border">
 		<div class="block-content">
 			<h4 class="widgettitle"><?php echo $title;?></h4>
-			<div class="widget widget_text" id="text-2"><?php echo $contents;?></div>
+			<?php echo $contents;?>
 		</div>
 	</div>
 </div>
