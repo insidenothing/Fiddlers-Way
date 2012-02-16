140e6ec8caf3fcfae538646ab7f202ec6f3b2c35
Patrick McGuire <insidenothing@gmail.com>
2012-02-16 11:14:23 -0500

    admin box

--- application/controllers/page.php
+++ application/controllers/page.php
@@ -7,6 +7,14 @@ class Page extends CI_Controller {
 		$this->load->model('page_model','page');
 		$data['title'] = $this->page->get_title($seo);
 		$data['contents'] = $this->page->get_contents($seo);
+		
+		if ($this->input->cookie('level') == 'Operator')
+		{
+			$data['contents'] = $this->input->cookie('name');
+		}else{
+			$data['operator'] = '';
+		}
+		
 		$this->load->library('Menu','menu');
 		$this->menu->load_common('page_view',$data);
 	}
