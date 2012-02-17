1cb7efec97829782ede69e29f6b51aa82b2ac52a
Patrick McGuire <insidenothing@gmail.com>
2012-02-17 11:57:58 -0500

    ooo

--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -12,7 +12,7 @@ class Admin_model extends CI_Model {
 	{
 		$rows='';
 		$query = $this->db->query("SELECT * from $table order by id DESC");
-		if ($query->num_rows() == 0)
+		if ($query->num_rows() > 0)
 		{
 			foreach ($query->result() as $row)
 			{
@@ -23,67 +23,6 @@ class Admin_model extends CI_Model {
 		return $rows;
 	}
 	
-	
-	function get_title($id)
-	{
-		if ($id > 0)
-		{
-			$query = $this->db->query("SELECT title from blogs where id = '$id'");
-			if ($query->num_rows() == 0)
-			{
-				$query = $this->db->query("SELECT title from blogs order by id DESC limit 0,1");
-			}
-		}
-		$row = $query->row();
-		$query->free_result();
-		return $row->title;
-	}
-
-	function get_author($id)
-	{
-		if ($id > 0)
-		{
-			$query = $this->db->query("SELECT author from blogs where id = '$id'");
-			if ($query->num_rows() == 0)
-			{
-				$query = $this->db->query("SELECT author from blogs order by id DESC limit 0,1");
-			}
-		}
-		$row = $query->row();
-		$query->free_result();
-		return $row->author;
-	}
-
-	function get_published($id)
-	{
-		if ($id > 0)
-		{
-			$query = $this->db->query("SELECT published from blogs where id = '$id'");
-			if ($query->num_rows() == 0)
-			{
-				$query = $this->db->query("SELECT published from blogs order by id DESC limit 0,1");
-			}
-		}
-		$row = $query->row();
-		$query->free_result();
-		return $row->published;
-	}
-
-	function get_contents($id)
-	{
-			if ($id > 0)
-		{
-			$query = $this->db->query("SELECT content from blogs where id = '$id'");
-			if ($query->num_rows() == 0)
-			{
-				$query = $this->db->query("SELECT content from blogs order by id DESC limit 0,1");
-			}
-		}
-		$row = $query->row();
-		$query->free_result();
-		return $row->content;
-	}
-
 	
 	
 }
