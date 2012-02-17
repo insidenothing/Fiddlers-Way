32b00b46a626058d2bcddf56b09a9668aff32598
Patrick McGuire <insidenothing@gmail.com>
2012-02-17 08:49:18 -0500

    ok

--- application/controllers/press_releases.php
+++ application/controllers/press_releases.php
@@ -1,29 +1,19 @@
 
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Wire extends CI_Controller {
+class Press_releases extends CI_Controller {
 
 	public function index()
 	{
 		$this->load->model('wire_model','wire');
-		$data['title'] = $this->wire->get_title($seo);
-		$data['contents'] = $this->wire->get_contents($seo);
-		$data['id'] = $this->wire->get_id($seo);
-
-
-
-
+		
 		/* most recent blog posting */
 
 		$data['title1'] = $this->wire->get_title('press_release_2');
-		$data['author1'] = $this->wire->get_author('press_release_2');
-		$data['published1'] = $this->wire->get_published('press_release_2');
 		$data['contents1'] = $this->wire->get_contents('press_release_2');
 
 
 		$data['title2'] = $this->wire->get_title('press_release_1');
-		$data['author2'] = $this->wire->get_author('press_release_1');
-		$data['published2'] = $this->wire->get_published('press_release_1');
 		$data['contents2'] = $this->wire->get_contents('press_release_1');
 
 
