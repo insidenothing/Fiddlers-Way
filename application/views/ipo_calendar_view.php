c6ccf13f45e2450c0d2347cbc445e930e81625a9
Patrick McGuire <insidenothing@gmail.com>
2012-02-19 11:10:52 -0500

    sortable

--- application/views/ipo_calendar_view.php
+++ application/views/ipo_calendar_view.php
@@ -1,28 +1,42 @@
+<script src="/assets/js/sorttable.js"></script>
+<style>
+/* Sortable tables */
+table.sortable thead {
+    background-color:#eee;
+    color:#666666;
+    font-weight: bold;
+    cursor: default;
+}
+</style>
+
+
 <div class="block" style="margin-top: 5px; width:1100px !important;">
 	<div class="block-border">
 		<div class="block-content">
 			<h4 class="widgettitle">IPO CALENDAR</h4>
 			<div class="widget widget_text" id="text-2">
-			<table>
+			<table class="sortable" cellspacing="0" cellpadding="2" border="1" width="100%" style="border-colapse:colaspe;">
+				<thead>
 				<tr>
-					<td style="white-space: pre">PUBLISHED</td>
-					<td style="white-space: pre">NAME</td>
-					<td>manager</td>
-					<td style="white-space: pre">catagory</td>
-					<td style="white-space: pre">shares_mm</td>
+					<td style="white-space: pre">Published</td>
+					<td style="white-space: pre">Name</td>
+					<td>Manager</td>
+					<td style="white-space: pre">Catagory</td>
+					<td style="white-space: pre">Shares (mm)</td>
 					
-					<td style="white-space: pre">price_low</td>
-					<td style="white-space: pre">pre_ipo_price</td>
-					<td style="white-space: pre">price_high</td>
+					<td style="white-space: pre">Price Low</td>
+					<td style="white-space: pre">Pre IPO Price</td>
+					<td style="white-space: pre">Price High</td>
 					
-					<td style="white-space: pre">pre_ipo_amount_mm</td>
-					<td style="white-space: pre">estimate</td>
+					<td style="white-space: pre">Pre IPO Amount (mm)</td>
+					<td style="white-space: pre">Estimate</td>
 					
-					<td style="white-space: pre">expected</td>
+					<td style="white-space: pre">Expected</td>
 					<td style="white-space: pre">40 Day</td>
 					<td style="white-space: pre">180 Day</td>
 					
 				</tr>
+				</thead>
 				<?php echo $ipos?>
 			</table>
 			</div>
