4e0872740694284de479b532f4c11a717732a9c1
Patrick McGuire <insidenothing@gmail.com>
2012-02-20 10:45:07 -0500

    roll out functions

--- application/controllers/video.php
+++ application/controllers/video.php
@@ -8,5 +8,29 @@ class Video extends CI_Controller {
 		$this->load->view('video_view');
 		$this->load->view('common_footer');
 	}
+	
+	public function archive()
+	{
+		$this->load->view('common_header');
+		$this->load->view('video_view');
+		$this->load->view('common_footer');
+	}
+	
+	
+	public function blog()
+	{
+		$this->load->view('common_header');
+		$this->load->view('video_view');
+		$this->load->view('common_footer');
+	}
+	
+	public function news()
+	{
+		$this->load->view('common_header');
+		$this->load->view('video_view');
+		$this->load->view('common_footer');
+	}
+	
+	
 }
 
