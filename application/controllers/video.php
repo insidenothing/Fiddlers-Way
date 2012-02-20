8f03c4e2ae02550aaf6dfd1c8582a53d4500980e
Patrick McGuire <insidenothing@gmail.com>
2012-02-20 11:58:01 -0500

    blog and news videos online

--- application/controllers/video.php
+++ application/controllers/video.php
@@ -28,11 +28,8 @@ class Video extends CI_Controller {
 	public function archive()
 	{
 		$this->load->model('video_model','video');
-		
-		
 		$data['pages'] = $this->video->get_list('archive');
 		$data['catagory'] = 'archive';
-		
 		$this->load->library('Menu','menu');
 		$this->menu->load_common('video_list_view',$data);
 	}
@@ -42,46 +39,19 @@ class Video extends CI_Controller {
 	{
 
 		$this->load->model('video_model','video');
-		$data['seo'] = $this->video->get_seo();
-		$data['title'] = $this->video->get_title($data['seo']);
-		$data['contents'] = $this->video->get_contents($data['seo']);
-		$data['comments'] = $this->video->get_comments($data['seo']);
-		$data['id'] = $this->video->get_id($data['seo']);
-		
-
-		if ($this->input->cookie('level') == 'Operator')
-		{
-			$data['operator'] = $this->input->cookie('name');
-		}else{
-			$data['operator'] = '';
-		}
-
+		$data['pages'] = $this->video->get_list('blog');
+		$data['catagory'] = 'blog';
 		$this->load->library('Menu','menu');
-		$this->menu->load_common('video_view',$data);
+		$this->menu->load_common('video_list_view',$data);
 	}
 
 	public function news()
 	{
-
 		$this->load->model('video_model','video');
-		$data['seo'] = $this->video->get_seo();
-		$data['title'] = $this->video->get_title($data['seo']);
-		$data['contents'] = $this->video->get_contents($data['seo']);
-		$data['comments'] = $this->video->get_comments($data['seo']);
-		$data['id'] = $this->video->get_id($data['seo']);
-		
-
-		if ($this->input->cookie('level') == 'Operator')
-		{
-			$data['operator'] = $this->input->cookie('name');
-		}else{
-			$data['operator'] = '';
-		}
-
+		$data['pages'] = $this->video->get_list('news');
+		$data['catagory'] = 'news';
 		$this->load->library('Menu','menu');
-		$this->menu->load_common('video_view',$data);
+		$this->menu->load_common('video_list_view',$data);
 	}
-
-
 }
 
