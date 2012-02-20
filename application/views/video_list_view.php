b158ab3d3eb75f53c4ff8b6aa5c11e99094e878e
Patrick McGuire <insidenothing@gmail.com>
2012-02-20 11:55:53 -0500

    better

--- application/views/video_list_view.php
+++ application/views/video_list_view.php
@@ -2,7 +2,7 @@
 	<div class="block-border">
 		<div class="block-content">
 			<div class="widget widget_text" id="text-2">
-				<h4 class="widgettitle"><?php echo $catagory;?></h4>
+				<h4 class="widgettitle">Videos: <?php echo $catagory;?></h4>
 				
 				<table border="1" cellpadding="2" cellspacing="0">
 				<tr>
