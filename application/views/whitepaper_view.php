145b6b3800fabe7ddfd67ab96a18127ed24dc514
Patrick McGuire <insidenothing@gmail.com>
2012-02-21 10:33:40 -0500

    better

--- application/views/whitepaper_view.php
+++ application/views/whitepaper_view.php
@@ -1,7 +1,7 @@
 <div class="block" style="margin-top: 5px; width:1000px !important;">
 	<div class="block-border">
 		<div class="block-content">
-			<h4 class="widgettitle"><?php echo $title;?></h4>
+			<h4 class="widgettitle">WHITE PAPER: <?php echo $title;?></h4>
 			<div class="widget widget_text" id="text-2"><?php echo $contents;?></div>
 		</div>
 	</div>
