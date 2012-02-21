e8a8b94c6e5a4c0263f466de35037460507fa3fa
Patrick McGuire <insidenothing@gmail.com>
2012-02-20 21:32:24 -0500

    multiple buttons

--- application/views/sales_view.php
+++ application/views/sales_view.php
@@ -9,30 +9,69 @@ Recommend to your friends so you may help each other.
 </div>
 
 <div>Member Benefits</div>
-<div>You'll get full site access including IPOreports — with early emailed buy/avoid IPO recommendations, 
-pre & post IPO  — with special emphasis on HIGH YIELDING IPOs.</div>
+<div>You'll get full site access including IPOreports - with early emailed buy/avoid IPO recommendations, 
+pre & post IPO  - with special emphasis on HIGH YIELDING IPOs.</div>
+
+
+
+
+<br><br>
+<center>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="S2KFMD6XY7X9L">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+</center>
+
+<br><br>
+
+
 
 <div>You'll make better investment decisions
 with unique compare/contrast valuations,
 developed over 15 years based on financial analysis.</div>
-
+<br>
 <div>We are the ONLY IPO research service
 that analyzes/grades/scores each & every IPO filed with the SEC.</div>
-
+<br>
 <div>Two week free trial,  see below</div>
+<br>
+<div>And yes, you CAN make money in the IPO after market.  Four fairly recent examples...</div>
 
-<div>And yes, you CAN make money in the IPO after market.  Four fairly recent examples…</div>
+<br><br>
 
 <div>HIGH YIELD PLAYS</div>
+
 <div>+43% A favorite IPO from 2011 is Chesapeake Granite Wash Trust (CHKR) which IPO'd November 10, 2011 at $19 traded
  down a little and today is $27.27, up 43%.  And at $19 CHKR was projected to pay a yield in 2012 of 13%
 CHKR  quote  IPOreport</div>
+
+<br><br>
 
 <div>+80% We always liked CVR Partners (UAN), because of it yield and its business.  UAN IPO'd April 7, 2011 at $16, and
  now trades up 80% at $28.87.  You could have bought it in the IPO after market for $16.75  At the pre-IPO price range 
  mid-point of $13, UAN was expected to yield 14.8%.
 UAN  quote  IPOreport
 </div>
+
+<br><br>
+
+<center>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="S2KFMD6XY7X9L">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+</center>
+
+<br><br>
+
+
 
 <div>+144% TECHNOLOGY PLAY<br>
 One of our highest scored stocks of 2011 was InvenSense (INVN)
