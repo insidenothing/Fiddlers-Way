04c80bb7d42072bf65f6c85a10a0495015c3ec4d
Patrick McGuire <insidenothing@gmail.com>
2012-02-21 10:19:42 -0500

    whitepaper

---
+++ application/controllers/whitepaper.php
@@ -0,0 +1,23 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Whitepaper extends CI_Controller {
+
+	public function index($seo)
+	{
+		$this->load->model('whitepaper_model','whitepaper');
+		$data['title'] = $this->whitepaper->get_title($seo);
+		$data['contents'] = $this->whitepaper->get_contents($seo);
+		$data['id'] = $this->whitepaper->get_id($seo);
+		
+		if ($this->input->cookie('level') == 'Operator')
+		{
+			$data['operator'] = $this->input->cookie('name');
+		}else{
+			$data['operator'] = '';
+		}
+		
+		$this->load->library('Menu','menu');
+		$this->menu->load_common('whitepaper_view',$data);
+	}
+}
+
---
+++ application/views/whitepaper_view.php
@@ -0,0 +1,24 @@
+<div class="block" style="margin-top: 5px;">
+	<div class="block-border">
+		<div class="block-content">
+			<h4 class="widgettitle"><?php echo $title;?></h4>
+			<div class="widget widget_text" id="text-2"><?php echo $contents;?></div>
+		</div>
+	</div>
+</div>
+
+<?php if ($operator != ''){ ?>
+
+<div class="block" style="margin-top: 5px;">
+<div class="block-border">
+<div class="block-content">
+<h4 class="widgettitle">Operator: <?php echo $operator;?></h4>
+			<div class="widget widget_text" id="text-2">
+				<li><a href="/edit/index/whitepapers/<?php echo $id;?>">Edit White Paper</a></li>
+			</div>
+		</div>
+	</div>
+</div>
+
+	
+<?php }?>
