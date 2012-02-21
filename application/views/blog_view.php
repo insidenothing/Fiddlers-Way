a4bb667a258af0746f363d085a5099c836a380f7
Patrick McGuire <insidenothing@gmail.com>
2012-02-21 14:39:23 -0500

    edit

--- application/views/blog_view.php
+++ application/views/blog_view.php
@@ -8,3 +8,20 @@
 		</div>
 	</div>
 </div>
+
+
+<?php if ($operator != ''){ ?>
+
+<div class="block" style="margin-top: 5px;">
+<div class="block-border">
+<div class="block-content">
+<h4 class="widgettitle">Operator: <?php echo $operator;?></h4>
+			<div class="widget widget_text" id="text-2">
+				<li><a href="/edit/index/blogs/<?php echo $id;?>">Edit Blog</a></li>
+			</div>
+		</div>
+	</div>
+</div>
+
+	
+<?php }?>
