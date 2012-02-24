34cc4e4f21e759829dd9db239e81caf8d3c30436
Patrick McGuire <insidenothing@gmail.com>
2012-02-24 12:35:07 -0500

    fully live

--- application/controllers/ipo.php
+++ application/controllers/ipo.php
@@ -39,7 +39,7 @@ class Ipo extends CI_Controller {
 		}
 
 
-		/* Record Updates */
+		/* check for an updated symbol */
 		if ($this->input->post('symbol')){
 			$results .= $this->ipo->set_ipo_data('symbol',$symbol,$this->input->post('symbol'));
 			$data['symbol'] = $this->input->post('symbol');
@@ -47,21 +47,94 @@ class Ipo extends CI_Controller {
 			$data['symbol'] = $symbol;
 		}
 		
-		
+		/* Record Updates */
 		if ($this->input->post('premium_report')){
 			$results .= $this->ipo->set_ipo_data('premium_report',$data['symbol'],$this->input->post('premium_report'));
 		}
 		if ($this->input->post('published')){
 			$results .= $this->ipo->set_ipo_data('published',$data['symbol'],$this->input->post('published'));
 		}
+		if ($this->input->post('published_date')){
+			$results .= $this->ipo->set_ipo_data('published_date',$data['symbol'],$this->input->post('published_date'));
+		}
+		if ($this->input->post('name')){
+			$results .= $this->ipo->set_ipo_data('name',$data['symbol'],$this->input->post('name'));
+		}
+		if ($this->input->post('notes')){
+			$results .= $this->ipo->set_ipo_data('notes',$data['symbol'],$this->input->post('notes'));
+		}
+		if ($this->input->post('manager')){
+			$results .= $this->ipo->set_ipo_data('manager',$data['symbol'],$this->input->post('manager'));
+		}
+		if ($this->input->post('catagory')){
+			$results .= $this->ipo->set_ipo_data('catagory',$data['symbol'],$this->input->post('catagory'));
+		}
+		if ($this->input->post('shares_mm')){
+			$results .= $this->ipo->set_ipo_data('shares_mm',$data['symbol'],$this->input->post('shares_mm'));
+		}
+		if ($this->input->post('price_high')){
+			$results .= $this->ipo->set_ipo_data('price_high',$data['symbol'],$this->input->post('price_high'));
+		}
+		if ($this->input->post('pre_ipo_price')){
+			$results .= $this->ipo->set_ipo_data('pre_ipo_price',$data['symbol'],$this->input->post('pre_ipo_price'));
+		}
+		if ($this->input->post('price_low')){
+			$results .= $this->ipo->set_ipo_data('price_low',$data['symbol'],$this->input->post('price_low'));
+		}
+		if ($this->input->post('estimate')){
+			$results .= $this->ipo->set_ipo_data('estimate',$data['symbol'],$this->input->post('estimate'));
+		}
+		if ($this->input->post('pre_ipo_amount_mm')){
+			$results .= $this->ipo->set_ipo_data('pre_ipo_amount_mm',$data['symbol'],$this->input->post('pre_ipo_amount_mm'));
+		}
+		if ($this->input->post('expected')){
+			$results .= $this->ipo->set_ipo_data('expected',$data['symbol'],$this->input->post('expected'));
+		}
+		if ($this->input->post('rating_public')){
+			$results .= $this->ipo->set_ipo_data('rating_public',$data['symbol'],$this->input->post('rating_public'));
+		}
+		if ($this->input->post('recommendation_public')){
+			$results .= $this->ipo->set_ipo_data('recommendation_public',$data['symbol'],$this->input->post('recommendation_public'));
+		}
+		if ($this->input->post('day40')){
+			$results .= $this->ipo->set_ipo_data('day40',$data['symbol'],$this->input->post('day40'));
+		}
+		if ($this->input->post('day180')){
+			$results .= $this->ipo->set_ipo_data('day180',$data['symbol'],$this->input->post('day180'));
+		}
+		if ($this->input->post('rating_paid')){
+			$results .= $this->ipo->set_ipo_data('rating_paid',$data['symbol'],$this->input->post('rating_paid'));
+		}
+		if ($this->input->post('recommendation_paid')){
+			$results .= $this->ipo->set_ipo_data('recommendation_paid',$data['symbol'],$this->input->post('recommendation_paid'));
+		}
+		
 		
 
 		/* Public Data */
 		$data['published'] = $this->ipo->get_ipo_data('published',$data['symbol']);
-
+		$data['published_date'] = $this->ipo->get_ipo_data('published_date',$data['symbol']);
+		$data['name'] = $this->ipo->get_ipo_data('name',$data['symbol']);
+		$data['notes'] = $this->ipo->get_ipo_data('notes',$data['symbol']);
+		$data['manager'] = $this->ipo->get_ipo_data('manager',$data['symbol']);
+		$data['catagory'] = $this->ipo->get_ipo_data('catagory',$data['symbol']);
+		$data['shares_mm'] = $this->ipo->get_ipo_data('shares_mm',$data['symbol']);
+		$data['price_high'] = $this->ipo->get_ipo_data('price_high',$data['symbol']);
+		$data['pre_ipo_price'] = $this->ipo->get_ipo_data('pre_ipo_price',$data['symbol']);
+		$data['price_low'] = $this->ipo->get_ipo_data('price_low',$data['symbol']);
+		$data['estimate'] = $this->ipo->get_ipo_data('estimate',$data['symbol']);
+		$data['pre_ipo_amount_mm'] = $this->ipo->get_ipo_data('pre_ipo_amount_mm',$data['symbol']);
+		$data['expected'] = $this->ipo->get_ipo_data('expected',$data['symbol']);
+		$data['rating_public'] = $this->ipo->get_ipo_data('rating_public',$data['symbol']);
+		$data['recommendation_public'] = $this->ipo->get_ipo_data('recommendation_public',$data['symbol']);
+		$data['day40'] = $this->ipo->get_ipo_data('day40',$data['symbol']);
+		$data['day180'] = $this->ipo->get_ipo_data('day180',$data['symbol']);
+		
 		/* Premium Data */
 		$data['premium_report'] = $this->ipo->get_ipo_data('premium_report',$data['symbol']);
-
+		$data['rating_paid'] = $this->ipo->get_ipo_data('rating_paid',$data['symbol']);
+		$data['recommendation_paid'] = $this->ipo->get_ipo_data('recommendation_paid',$data['symbol']);
+		
 		/* common data */
 		$data['results'] = $results;
 
--- application/views/ipo_edit_view.php
+++ application/views/ipo_edit_view.php
@@ -1,56 +1,145 @@
 
-<script
-	language="JavaScript" type="text/javascript"
-	src="/assets/wysiwyg/wysiwyg.js"></script>
+<script language="JavaScript"
+	type="text/javascript" src="/assets/wysiwyg/wysiwyg.js"></script>
 
-<form method="POST"
-	action="/ipo/edit/<?php echo $symbol; ?>">
+<form method="POST" action="/ipo/edit/<?php echo $symbol; ?>">
 
 
 
-<div class="block" style="margin-top: 5px; width:1000px !important;">
-	<div class="block-border">
-		<div class="block-content">
+	<div class="block" style="margin-top: 5px; width: 1000px !important;">
+		<div class="block-border">
+			<div class="block-content">
 
-			<div class="widget widget_text" id="text-2">
-			<h4 class="widgettitle">Operator: <?php echo $operator;?></h4>
-			<h4>Settings: <?php echo $symbol;?></h4>
-			<table width="100%">
-				
-				<tr>
-					<td>Symbol</td>
-					<td><input name="symbol" value="<?php echo $symbol;?>"></td>
-				</tr>
-				<tr>
-					<td>Published</td>
-					<td><input name="published" value="<?php echo $published;?>"></td>
-				</tr>
-				
-				<tr bgcolor="#FFDE53">
-					<td><input type="submit" value="Save Updated IPO Information"></td>
-					<td><b><?php echo $results;?></b></td>
-				</tr>
-				<tr>
-					
-					<td colspan="2"><a href="/ipo/index/<?php echo $symbol;?>"><b>Click this link to view <?php echo $symbol;?></b></a></td>
-				</tr>
-			</table>				
-	
-				
+				<div class="widget widget_text" id="text-2">
+					<h4 class="widgettitle">
+						Operator:
+						<?php echo $operator;?>
+					</h4>
+					<h4>
+						Settings:
+						<?php echo $symbol;?>
+					</h4>
+					<table width="100%">
+
+						<tr>
+							<td>Symbol</td>
+							<td><input name="symbol" value="<?php echo $symbol;?>"></td>
+						</tr>
+						<tr>
+							<td>Published</td>
+							<td><input name="published" value="<?php echo $published;?>"></td>
+						</tr>
+						<tr>
+							<td>published_date</td>
+							<td><input name="published_date"
+								value="<?php echo $published_date;?>"></td>
+						</tr>
+						<tr>
+							<td>name</td>
+							<td><input name="name" value="<?php echo $name;?>"></td>
+						</tr>
+						<tr>
+							<td>notes</td>
+							<td><input name="notes" value="<?php echo $notes;?>"></td>
+						</tr>
+						<tr>
+							<td>manager</td>
+							<td><input name="manager" value="<?php echo $manager;?>"></td>
+						</tr>
+						<tr>
+							<td>catagory</td>
+							<td><input name="catagory" value="<?php echo $catagory;?>"></td>
+						</tr>
+						<tr>
+							<td>shares_mm</td>
+							<td><input name="shares_mm" value="<?php echo $shares_mm;?>"></td>
+						</tr>
+						<tr>
+							<td>price_high</td>
+							<td><input name="price_high" value="<?php echo $price_high;?>"></td>
+						</tr>
+						<tr>
+							<td>pre_ipo_price</td>
+							<td><input name="pre_ipo_price"
+								value="<?php echo $pre_ipo_price;?>"></td>
+						</tr>
+						<tr>
+							<td>price_low</td>
+							<td><input name="price_low" value="<?php echo $price_low;?>"></td>
+						</tr>
+						<tr>
+							<td>estimate</td>
+							<td><input name="estimate" value="<?php echo $estimate;?>"></td>
+						</tr>
+						<tr>
+							<td>pre_ipo_amount_mm</td>
+							<td><input name="pre_ipo_amount_mm"
+								value="<?php echo $pre_ipo_amount_mm;?>"></td>
+						</tr>
+						<tr>
+							<td>expected</td>
+							<td><input name="expected" value="<?php echo $expected;?>"></td>
+						</tr>
+						<tr>
+							<td>rating_public</td>
+							<td><input name="rating_public"
+								value="<?php echo $rating_public;?>"></td>
+						</tr>
+						<tr>
+							<td>recommendation_public</td>
+							<td><input name="recommendation_public"
+								value="<?php echo $recommendation_public;?>"></td>
+						</tr>
+						<tr>
+							<td>day40</td>
+							<td><input name="day40" value="<?php echo $day40;?>"></td>
+						</tr>
+						<tr>
+							<td>day180</td>
+							<td><input name="day180" value="<?php echo $day180;?>"></td>
+						</tr>
+
+
+						<tr bgcolor="#FFDE53">
+							<td><input type="submit" value="Save Updated IPO Information"></td>
+							<td><b><?php echo $results;?> </b></td>
+						</tr>
+						<tr>
+
+							<td colspan="2"><a href="/ipo/index/<?php echo $symbol;?>"><b>Click
+										this link to view <?php echo $symbol;?> </b> </a></td>
+						</tr>
+					</table>
+
+
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
 
 
 
-	<div class="block" style="margin-top: 5px; width:1000px !important;">
+	<div class="block" style="margin-top: 5px; width: 1000px !important;">
 		<div class="block-border">
 			<div class="block-content">
-<h4 class="widgettitle">Premium Report</h4>
+				<h4 class="widgettitle">Premium Content</h4>
+				<table width="100%">
+					<tr>
+						<td>rating_paid</td>
+						<td><input name="rating_paid" value="<?php echo $rating_paid;?>"></td>
+					</tr>
+					<tr>
+						<td>recommendation_paid</td>
+						<td><input name="recommendation_paid" value="<?php echo $recommendation_paid;?>"></td>
+					</tr>
+				</table>
+
 				<div class="widget widget_text" id="text-2">
 
-					<textarea id="whiteboard" rows="30" cols="100" name="premium_report">
+					<textarea id="whiteboard" rows="30" cols="100"
+						name="premium_report">
+						
+						
 					<?PHP echo $premium_report;?></textarea>
 
 					<script language="JavaScript">
