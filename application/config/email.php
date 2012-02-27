bcf5fa268cf34171851fb5eb2e680f3027ad36f0
Patrick McGuire <insidenothing@gmail.com>
2012-02-27 16:23:54 -0500

    back to patrick...

--- application/config/email.php
+++ application/config/email.php
@@ -5,6 +5,6 @@ $config['mailtype'] = 'html';
 $config['protocol'] = 'smtp'; 
 
 $config['smtp_host'] = 'ssl://smtp.googlemail.com';
-$config['smtp_user'] = 'no-reply@fiddlersway.com';
+$config['smtp_user'] = 'patrick@fiddlersway.com';
 $config['smtp_pass'] = 'lox2stay';
 $config['smtp_port'] = '465';
