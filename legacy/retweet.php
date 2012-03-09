f72c4d2e394828ce31799976722f2a598b0e8862
Patrick McGuire <insidenothing@gmail.com>
2012-03-09 15:52:42 -0500

    legacy toy

--- legacy/retweet.php
+++ legacy/retweet.php
@@ -1,43 +1,26 @@
 <?php
-/**
-* post_tweet.php
-* Example of posting a tweet with OAuth
-* Latest copy of this code: 
-* http://140dev.com/twitter-api-programming-tutorials/hello-twitter-oauth-php/
-* @author Adam Green <140dev@gmail.com>
-* @license GNU Public License
-*/
 
-$tweet_text = 'Hello Twitter';
-print "Posting...\n";
-$result = post_tweet($tweet_text);
-print "Response code: " . $result . "\n";
 
-function post_tweet($tweet_text) {
+  require 'tmhOAuth.php';
+  require 'tmhUtilities.php';
 
-  // Use Matt Harris' OAuth library to make the connection
-  // This lives at: https://github.com/themattharris/tmhOAuth
-  require_once('tmhOAuth.php');
-      
-  // Set the authorization values
-  // In keeping with the OAuth tradition of maximum confusion, 
-  // the names of some of these values are different from the Twitter Dev interface
-  // user_token is called Access Token on the Dev site
-  // user_secret is called Access Token Secret on the Dev site
-  // The values here have asterisks to hide the true contents 
-  // You need to use the actual values from Twitter
-  $connection = new tmhOAuth(array(
+  $tmhOAuth = new tmhOAuth(array(
     'consumer_key' => 'zpZFKm7BdV4GdvmnZTeig',
     'consumer_secret' => 'oSkpQsZDulWZ3rkYY9GvcqwC7OM6eSRBnBEVKvD4JM',
     'user_token' => '40141316-8KWwBsK9ss4YfMqQsEAG2KN97GcHKGQZ7GfFx6yQC',
     'user_secret' => 'ScXgkCdYvpjaFa4cKODQt4Lx86Ob8iXBsQj0ne3Io',
-  )); 
+  ));
   
-  // Make the API call
-  $connection->request('POST', 
-    $connection->url('1/statuses/update'), 
-    array('status' => $tweet_text));
+  $code = $tmhOAuth->request('POST', $tmhOAuth->url('1/statuses/update'), array(
+    'status' => 'My Twitter Message'
+  ));
   
-  return $connection->response['code'];
-}
-?>
+  if ($code == 200) {
+  	tmhUtilities::pr(json_decode($tmhOAuth->response['response']));
+  } else {
+  	tmhUtilities::pr($tmhOAuth->response['response']);
+  }
+  
+  
+  
+  ?>
