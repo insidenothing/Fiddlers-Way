8adf4dbf5a5dbe7bd430a3c871b0c7863af48dde
Patrick McGuire <insidenothing@gmail.com>
2012-03-12 10:33:28 -0400

    rt

--- application/controllers/retweet.php
+++ application/controllers/retweet.php
@@ -52,65 +52,15 @@ class Retweet extends CI_Controller {
 	}
 	
 	
-	public function viewT()
-	{
-	
-		$buffer='';
-		$twitterRssFeedUrl =  "https://twitter.com/statuses/user_timeline/IPOdesktop.rss";
-		$twitterUsername = "ipodesktop";
-		$amountToShow = 20;
-		$twitterPosts = false;
-		$xml = @simplexml_load_file($twitterRssFeedUrl);
-		if(is_object($xml)){
-			foreach($xml->channel->item as $twit){
-				if(is_array($twitterPosts) && count($twitterPosts)==$amountToShow){
-					break;
-				}
-				$d['title'] = stripslashes(htmlentities($twit->title,ENT_QUOTES,'UTF-8'));
-				$description = stripslashes(htmlentities($twit->description,ENT_QUOTES,'UTF-8'));
-				if(strtolower(substr($description,0,strlen($twitterUsername))) == strtolower($twitterUsername)){
-					$description = substr($description,strlen($twitterUsername)+1);
-				}
-				$d['description'] = $description;
-				$d['pubdate'] = strtotime($twit->pubDate);
-				$d['guid'] = stripslashes(htmlentities($twit->guid,ENT_QUOTES,'UTF-8'));
-				$d['link'] = stripslashes(htmlentities($twit->link,ENT_QUOTES,'UTF-8'));
-				$twitterPosts[]=$d;
-			}
-		}else{
-			die('cannot connect to twitter feed');
-		}
-	
-		if(is_array($twitterPosts)){
-			$buffer .= '<ul>';
-			foreach($twitterPosts as $post){
-				$buffer .= '<li><p>'.$post['description'].'</p><p class="date">Posted On: '.date('l jS \of F Y h:i:s A',$post['pubdate']).'</p></li>';
-			}
-			$buffer .= '</ul>';
-		}else{
-			$buffer .= '<p>No Twitter posts have been made</p>';
-		}
-	
-		$data['response'] = $buffer;
-		$this->load->view('retweet_view',$data);
-	}
 	
 	
 	public function sendT()
 	{
 		
-		$tweet = "Hello CodeIgniter";
-		/*
-		$curl = curl_init();
-		curl_setopt ($curl, CURLOPT_URL, 'http://fiddlersway.com/legacy/retweet.php');
-		curl_setopt ($curl, CURLOPT_TIMEOUT, '5');
-		curl_setopt ($curl, CURLOPT_RETURNTRANSFER, '1');
-		curl_setopt ($curl, CURLOPT_POSTFIELDS, "message=$tweet");
-		$buffer = curl_exec ($curl);
-		curl_close ($curl);
-		$data['response'] = $buffer;
+		$this->load->model('retweet_model','twitter');
+		$data['response'] = $this->twitter->send_tweets();
 		$this->load->view('retweet_view',$data);
-		*/
+		
 	}
 	
 }
--- application/models/retweet_model.php
+++ application/models/retweet_model.php
@@ -17,13 +17,35 @@ class Retweet_model extends CI_Model {
 		if ($query->num_rows() == 0)
 		{
 			$query = $this->db->query("insert into twitter (status,tweet,note) values ('new','$tweet','$note')");
-			return "<li>Tweet Added</li>";
+			return " Tweet Added ";
 		}else{
-			return "<li>Known Tweet</li>";
+			return " Known Tweet ";
 		}
 	}
 	
-	
+	function send_tweets()
+	{
+		$return = '';
+		$query = $this->db->query("SELECT * from twitter where status = 'new' order by note ASC ");
+		if ($query->num_rows() == 0)
+		{
+			foreach ($query->result() as $row)
+			{
+				$curl = curl_init();
+				curl_setopt ($curl, CURLOPT_URL, 'http://fiddlersway.com/legacy/retweet.php');
+				curl_setopt ($curl, CURLOPT_TIMEOUT, '5');
+				curl_setopt ($curl, CURLOPT_RETURNTRANSFER, '1');
+				curl_setopt ($curl, CURLOPT_POSTFIELDS, "message=".$row->tweet);
+				$buffer = curl_exec ($curl);
+				curl_close ($curl);
+				$return .= $buffer;				
+				$query = $this->db->query("update twitter set status = 'sent' where id = '".$row->id."' ");
+			}
+			return " Tweets Sent: $return";
+		}else{
+			return " Nothing to Send ";
+		}
+	}
 	
 	
 }
