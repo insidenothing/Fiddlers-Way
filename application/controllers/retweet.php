e1279fa871446ea6e36c88bc86138292bc8642c6
Patrick McGuire <insidenothing@gmail.com>
2012-03-12 09:55:59 -0400

    pull twitter

--- application/controllers/retweet.php
+++ application/controllers/retweet.php
@@ -4,6 +4,45 @@ class Retweet extends CI_Controller {
 
 	public function index()
 	{
+		
+		$buffer='';
+		$twitterRssFeedUrl =  "http://twitter.com/statuses/user_timeline/47995476.rss";
+		$twitterUsername = "eccemedia";
+		$amountToShow = 5;
+		$twitterPosts = false;
+		$xml = @simplexml_load_file($twitterRssFeedUrl);
+		if(is_object($xml)){
+			foreach($xml->channel->item as $twit){
+				if(is_array($twitterPosts) && count($twitterPosts)==$amountToShow){
+					break;
+				}
+				$d['title'] = stripslashes(htmlentities($twit->title,ENT_QUOTES,'UTF-8'));
+				$description = stripslashes(htmlentities($twit->description,ENT_QUOTES,'UTF-8'));
+				if(strtolower(substr($description,0,strlen($twitterUsername))) == strtolower($twitterUsername)){
+					$description = substr($description,strlen($twitterUsername)+1);
+				}
+				$d['description'] = $description;
+				$d['pubdate'] = strtotime($twit->pubDate);
+				$d['guid'] = stripslashes(htmlentities($twit->guid,ENT_QUOTES,'UTF-8'));
+				$d['link'] = stripslashes(htmlentities($twit->link,ENT_QUOTES,'UTF-8'));
+				$twitterPosts[]=$d;
+			}
+		}else{
+			die('cannot connect to twitter feed');
+		}
+		
+		if(is_array($twitterPosts)){
+			$buffer .= '<ul>';
+			foreach($twitterPosts as $post){
+				$buffer .= '<li><p>'.$post['description'].'</p><p class="date">Posted On: '.date('l jS \of F Y h:i:s A',$post['pubdate']).'</p></li>';
+			}
+			$buffer .= '</ul>';
+		}else{
+			$buffer .= '<p>No Twitter posts have been made</p>';
+		}
+		
+		$data['response'] = $buffer;
+		
 		/*
 		$tweet = "Hello CodeIgniter";
 		
@@ -17,5 +56,6 @@ class Retweet extends CI_Controller {
 		$data['response'] = $buffer;
 		$this->load->view('retweet_view',$data);
 		*/
+		$this->load->view('retweet_view',$data);
 	}
 }
