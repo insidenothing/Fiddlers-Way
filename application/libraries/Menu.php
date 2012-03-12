6376a88e10dae0143dcbcf46c6557fadb87577b3
Patrick McGuire <insidenothing@gmail.com>
2012-03-12 19:21:01 -0400

    poic

--- application/libraries/Menu.php
+++ application/libraries/Menu.php
@@ -69,7 +69,7 @@ class Menu   {
 		$data['left_blog_recent'] = $CI->blog->get_blog_list('0','5');
 		$data['left_blog_rest'] = $CI->blog->get_blog_list('5','20');
 		
-		$data['right_news'] $this->news();
+		$data['right_news'] = $this->news();
 		
 		
 		$CI->load->view('common_header',$data);
