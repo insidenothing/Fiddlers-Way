12305598c63e3533a870665b61a3683bfc7fe0c9
Patrick McGuire <insidenothing@gmail.com>
2012-03-12 20:37:11 -0400

    ok

--- application/libraries/Menu.php
+++ application/libraries/Menu.php
@@ -100,7 +100,7 @@ class Menu   {
 
 
 		$buffer='';
-		$feed_url =  "http://www.google.com/alerts/feeds/11832344583767583685/17859790038354650960";
+		$feed_url =  "http://news.google.com/news?hl=en&gl=us&q=Francis+Gaskins&um=1&ie=UTF-8&output=rss";
 
 		$content = file_get_contents($feed_url);
 
