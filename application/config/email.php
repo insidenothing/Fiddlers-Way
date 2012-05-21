29e6e97fdeb81a6d5b005467f2b738cf01c21d7a
Patrick McGuire <insidenothing@gmail.com>
2012-05-21 13:08:28 -0400

    test non smtp relay
    Signed-off-by: Patrick McGuire <insidenothing@gmail.com>

--- application/config/email.php
+++ application/config/email.php
@@ -2,9 +2,10 @@
 
 $config['mailtype'] = 'html';
 
+/*
 $config['protocol'] = 'smtp'; 
-
 $config['smtp_host'] = 'ssl://smtp.googlemail.com';
 $config['smtp_user'] = 'patrick@fiddlersway.com';
 $config['smtp_pass'] = 'lox2stay';
 $config['smtp_port'] = '465';
+*/
