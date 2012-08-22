d6a3ed30a112d807cda17183797abf0fb3f9a7e7
Patrick McGuire <insidenothing@gmail.com>
2012-08-21 21:51:29 -0400

    remove expired video

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,42 +1,4 @@
 
-
-<div class="block" style="margin-top: 5px;">
-	<div class="block-border">
-		<div class="block-content">
-			<div class="widget widget_text" id="text-2">
-				<center>
-					<object id="cnbcplayer" height="380" width="400"
-						classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
-						codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0">
-						<param name="type" value="application/x-shockwave-flash" />
-						<param name="allowfullscreen" value="true" />
-						<param name="allowscriptaccess" value="always" />
-						<param name="quality" value="best" />
-						<param name="scale" value="noscale" />
-						<param name="wmode" value="transparent" />
-						<param name="bgcolor" value="#000000" />
-						<param name="salign" value="lt" />
-						<param name="flashVars" value="startTime=000" />
-						<param name="flashVars" value="endTime=000" />
-						<param name="movie"
-							value="http://plus.cnbc.com/rssvideosearch/action/player/id/3000070318/code/cnbcplayershare" />
-						<embed name="cnbcplayer"
-							PLUGINSPAGE="http://www.macromedia.com/go/getflashplayer"
-							allowfullscreen="true" allowscriptaccess="always"
-							bgcolor="#000000" height="380" width="400" quality="best"
-							wmode="transparent" scale="noscale" salign="lt"
-							src="http://plus.cnbc.com/rssvideosearch/action/player/id/3000070318/code/cnbcplayershare"
-							type="application/x-shockwave-flash" />
-					</object>
-					<img src="/assets/images/slogan-bg.png" width="400" height="25" />
-					<br> Francis Gaskins on CNBC analyzing facebook
-				</center>
-			</div>
-		</div>
-	</div>
-</div>
-
-
 <div class="entry">
 	<div class="border">
 		<div class="bottom">
